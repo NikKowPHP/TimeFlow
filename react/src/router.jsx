e68fd718512fbe46c7275b8a593d0cb2c22460dd
NikKowPHP <nik.kow@outlook.com>
2023-05-17 17:00:54 +0200

    define routes for views

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,7 +1,22 @@
 import {createBrowserRouter} from 'react-router-dom';
+import Login from './views/Login';
+import Users from './views/Users';
+import Signup from './views/Signup';
 
 const router = createBrowserRouter([
-	
+	{
+		path: '/login',
+		element: <Login />
+	}, 
+	{
+		path: '/signup',
+		element: <Signup/>
+	}, 
+	{
+		path: '/users',
+		element: <Users/>
+	}, 
+
 
 ]);
 

c76e1d71cdc5b3bf00c6c5c863ace42bfa9003ab
NikKowPHP <nik.kow@outlook.com>
2023-05-18 08:07:33 +0200

    add title to a login form

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -9,11 +9,12 @@ export default function Login() {
 		<div className='login-signup-form animated fadeInDown'>
 			<div className="form">
 				<form action="" onSubmit={onSubmit}>
+					<h1 className='title'>Login into your account</h1>
 					<input type="email" placeholder='Email' />
 					<input type="password" placeholder='Password' />
 					<button className='btn btn-block'>Sign in</button>
 					<p className='message'>
-						Not Registered? <Link to='/signup'>Create An Account</Link>
+						Not Registered? <Link to='/signup'>Create an account</Link>
 					</p>
 
 				</form>

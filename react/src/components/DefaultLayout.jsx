e73703cbdafa817be49ec596be89cba8a64a1b68
NikKowPHP <nik.kow@outlook.com>
2023-05-18 07:59:59 +0200

    create logged in info and layout

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
-import {useStateContext} from '../contexts/ContextProvider'
-import { Navigate, Outlet } from 'react-router-dom'
+import React from "react";
+import { useStateContext } from "../contexts/ContextProvider";
+import { Link, Navigate, Outlet } from "react-router-dom";
 
 function DefaultLayout() {
-	const {user, token} = useStateContext();
-	if(!token) {
-		return <Navigate to='/login' />
-	}
-	return (
-		<>
-		<Outlet />
-
-		</>
-	)
+  const { user, token } = useStateContext();
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
+  const onLogout = (ev) => {
+    ev.preventDefault();
+  }
+  return (
+    <div id="defaultLayout">
+      <aside>
+        <Link to={"/dashboard"}>Dashboard</Link>
+        <Link to={"/users"}>Users</Link>
+      </aside>
+      <div className="content">
+        <header>
+          <div>Header</div>
+          <div>{user.name}</div>
+          <a href="#" onClick={onLogout} className="btn btn-logout">Logout</a>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 }
 
-export default DefaultLayout
+export default DefaultLayout;

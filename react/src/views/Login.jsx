2459f984f68c3e8cf1b93c611208ce917eb2e529
NikKowPHP <nik.kow@outlook.com>
2023-05-18 08:00:12 +0200

    create login page layout

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Login() {
+	const onSubmit = (ev) => {
+		ev.preventDefault();
+	}
 	return (
-		<div>Login</div>
+		<div className='login-signup-form animated fadeInDown'>
+			<div className="form">
+				<form action="" onSubmit={onSubmit}>
+					<input type="email" placeholder='Email' />
+					<input type="password" placeholder='Password' />
+					<button className='btn btn-block'>Sign in</button>
+					<p className='message'>
+						Not Registered? <Link to='/signup'>Create An Account</Link>
+					</p>
+
+				</form>
+			</div>
+		</div>
 	)
 }

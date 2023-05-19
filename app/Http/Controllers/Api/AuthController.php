d15e79b8d8a30308fe00d7ce3d530abd5a429d62
NikKowPHP <nik.kow@outlook.com>
2023-05-19 07:11:14 +0200

    create authenticate controller

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+    public function signup(SignupRequest $signupRequest) {
+
+    }
+    public function login(LoginRequest $loginRequest) {
+
+    }
+    public function logout() {
+
+    }
+}

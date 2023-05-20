c311d8a41cd6b18d22caecda87d6da21e64ab113
NikKowPHP <nik.kow@outlook.com>
2023-05-20 08:36:30 +0200

    create store method for user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
-use App\Models\User;
 
 class UserController extends Controller
 {
@@ -14,7 +15,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        return UserResource::collection( 
+            User::query()->orderBy('id', 'desc')->paginate(10)
+        );
     }
 
     /**
@@ -22,7 +25,11 @@ class UserController extends Controller
      */
     public function store(StoreUserRequest $request)
     {
-        //
+        $data = $request->validated();
+        $data['password'] = bcrypt($data['password']);
+        $user = User::create($data);
+
+        return response(new UserResource($user), 201);
     }
 
     /**

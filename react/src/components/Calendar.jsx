48489914396873162d8189546dc57e66df211bdd
NikKowPHP <nik.kow@outlook.com>
2023-05-23 14:58:01 +0200

    create layout for calendar

--- react/src/components/Calendar.jsx
+++ react/src/components/Calendar.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import styles from '../styles/calendar.css';
 
 export default function Calendar() {
 	return (
-		<div className='wrapper'>
+		<div className='calendar-wrapper'>
 			<header>
-				<p className="current-date"></p>
-				<div className="icons">
+				<p className={styles.current-date}></p>
+				<div className={styles.icons}>
 					<span id="prev" class="material-symbols-rounded">chevron_left</span>
+					<span id="next" class="material-symbols-rounded">chevron_right</span>
 				</div>
 			</header>
+			<div className={styles.calendar}>
+				<ul className={styles.weeks}>
+					<li>Mon</li>
+					<li>Tue</li>
+					<li>Wed</li>
+					<li>Thu</li>
+					<li>Fri</li>
+					<li>Sat</li>
+					<li>Sun</li>
+				</ul>
+				<div className={days}></div>
+			</div>
 		</div>
 	)
 }

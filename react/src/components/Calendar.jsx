e56a98a45992c05b6daac3f6d9c00e8e5b0b1a66
NikKowPHP <nik.kow@outlook.com>
2023-05-23 16:08:56 +0200

    show current day in the calendar

--- react/src/components/Calendar.jsx
+++ react/src/components/Calendar.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/calendar.css'
 
 export default function Calendar() {
+
+	const [selectedDate, setSelectedDate] = useState(new Date());
+	const [currentYear, setCurrentYear] = useState(null);
+		const onClickPerv = () => {
+
+		}
+		const onClickNext = () => {
+
+		}
+		const handleDateClick = (date) => {
+			setSelectedDate(date);
+		}
+
+
+	const renderCalendar = () => {
+		const currentDate = new Date();
+		const currentMonth = currentDate.getMonth();
+		const currentYear = currentDate.getFullYear();
+		console.log(currentDate);
+		const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+		const firstDayIndex = new Date(currentYear, currentMonth, 1).getDay();
+		const calendarDays = [];
+
+		for(let i = 1; i <= daysInMonth; i++) {
+			calendarDays.push(
+				<li className={`${currentDate.getDate() === i ? 'active' : 'inactive'}`} key={i} onClick={() => handleDateClick(new Date(currentYear, currentMonth, i))}>{i}</li>
+			)
+		}
+		return calendarDays;
+	}
+
 	return (
 		<div className='calendar-wrapper'>
 			<header>
-				<p className="current-date"></p>
+				<p className="current-date">{currentYear}</p>
 				<div className="icons">
-					<span id="prev" class="material-symbols-rounded">chevron_left</span>
-					<span id="next" class="material-symbols-rounded">chevron_right</span>
+					<span onClick = {onClickPerv} className="material-symbols-rounded">chevron_left</span>
+					<span onClick={onClickNext} className="material-symbols-rounded">chevron_right</span>
 				</div>
 			</header>
 			<div className="calendar">
@@ -21,7 +52,11 @@ export default function Calendar() {
 					<li>Sat</li>
 					<li>Sun</li>
 				</ul>
-				<div className="days"></div>
+				<div className="days">
+					<ul>
+						{renderCalendar()}
+					</ul>
+					</div>
 			</div>
 		</div>
 	)

edb1c8cb604c1d1393790555690597a7fd7cdd85
NikKowPHP <nik.kow@outlook.com>
2023-05-24 07:28:28 +0200

    remake calendar rendering methods using state

--- react/src/components/Calendar.jsx
+++ react/src/components/Calendar.jsx
@@ -1,63 +1,109 @@
-import React, { useState } from 'react'
-import '../styles/calendar.css'
+import React, { useEffect, useState } from "react";
+import "../styles/calendar.css";
 
 export default function Calendar() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
-	const [selectedDate, setSelectedDate] = useState(new Date());
-	const [currentYear, setCurrentYear] = useState(null);
-		const onClickPerv = () => {
+  const currentDate = new Date();
+	const currentDateDay = currentDate.getDate();
+  const [year, setYear] = useState(currentDate.getFullYear());
+  const [month, setMonth] = useState(currentDate.getMonth());
+  const [dates, setDates] = useState([]);
 
+
+  const getMonthName = (month) => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    return months[month];
+  };
+
+	useEffect(() => {
+		getCurrentMonthDates();
+	}, [])
+
+	const getCurrentMonthDates = () => {
+		const firstDayOfMonth = new Date(year,month, 1);
+		const lastDayOfMonth = new Date(year, month + 1, 0);
+		const currentMonthDates = [];
+
+		for(let i = firstDayOfMonth.getDate(); i <= lastDayOfMonth.getDate(); i++) {
+			currentMonthDates.push(i);
 		}
-		const onClickNext = () => {
-
-		}
-		const handleDateClick = (date) => {
-			setSelectedDate(date);
-		}
-
-
-	const renderCalendar = () => {
-		const currentDate = new Date();
-		const currentMonth = currentDate.getMonth();
-		const currentYear = currentDate.getFullYear();
-		console.log(currentDate);
-		const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
-		const firstDayIndex = new Date(currentYear, currentMonth, 1).getDay();
-		const calendarDays = [];
-
-		for(let i = 1; i <= daysInMonth; i++) {
-			calendarDays.push(
-				<li className={`${currentDate.getDate() === i ? 'active' : 'inactive'}`} key={i} onClick={() => handleDateClick(new Date(currentYear, currentMonth, i))}>{i}</li>
-			)
-		}
-		return calendarDays;
+		setDates(currentMonthDates);
 	}
 
-	return (
-		<div className='calendar-wrapper'>
-			<header>
-				<p className="current-date">{currentYear}</p>
-				<div className="icons">
-					<span onClick = {onClickPerv} className="material-symbols-rounded">chevron_left</span>
-					<span onClick={onClickNext} className="material-symbols-rounded">chevron_right</span>
-				</div>
-			</header>
-			<div className="calendar">
-				<ul className="weeks">
-					<li>Mon</li>
-					<li>Tue</li>
-					<li>Wed</li>
-					<li>Thu</li>
-					<li>Fri</li>
-					<li>Sat</li>
-					<li>Sun</li>
-				</ul>
-				<div className="days">
-					<ul>
-						{renderCalendar()}
-					</ul>
-					</div>
-			</div>
-		</div>
-	)
+  const getNextMonthDates = () => {
+    const nextMonth = new Date(year, month + 1, 1);
+    const nextMonthDates = [];
+
+    while (nextMonth.getMonth() === month + 1) {
+      nextMonthDates.push(nextMonth.getDate());
+      nextMonth.setDate(nextMonth.getDate() + 1);
+    }
+    debugger;
+    setDates(nextMonthDates);
+  };
+  const getPrevMonthDates = () => {};
+
+
+  const handleDateClick = (date) => {
+    setSelectedDate(date);
+  };
+
+
+
+  return (
+    <div className="calendar-wrapper">
+      <header>
+        <p className="current-date">{year}</p>
+        <p className="current-date">{getMonthName(month)}</p>
+        <div className="icons">
+          <span
+            onClick={getPrevMonthDates}
+            className="material-symbols-rounded"
+          >
+            chevron_left
+          </span>
+          <span
+            onClick={getNextMonthDates}
+            className="material-symbols-rounded"
+          >
+            chevron_right
+          </span>
+        </div>
+      </header>
+      <div className="calendar">
+        <ul className="weeks">
+          <li>Mon</li>
+          <li>Tue</li>
+          <li>Wed</li>
+          <li>Thu</li>
+          <li>Fri</li>
+          <li>Sat</li>
+          <li>Sun</li>
+        </ul>
+        <div className="days">
+          <ul>
+						{
+							dates.map((date, index)=> (
+								<li onClick={handleDateClick} className={currentDateDay === date ? 'active' : 'inactive' }  key={index}>{date}</li>
+							))
+						}
+						</ul>
+        </div>
+      </div>
+    </div>
+  );
 }

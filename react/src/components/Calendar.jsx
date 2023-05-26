8db0df8987ed52c582e1f97126668e14325b5757
NikKowPHP <nik.kow@outlook.com>
2023-05-26 08:30:14 +0200

    create show all month of the year, create layout for all month

--- react/src/components/Calendar.jsx
+++ react/src/components/Calendar.jsx
@@ -5,11 +5,12 @@ export default function Calendar() {
   const [selectedDate, setSelectedDate] = useState(new Date());
 
   const currentDate = new Date();
-	const currentDateDay = currentDate.getDate();
+  const currentDateDay = currentDate.getDate();
+  const months = generateMonths();
   const [year, setYear] = useState(currentDate.getFullYear());
   const [month, setMonth] = useState(currentDate.getMonth());
   const [dates, setDates] = useState([]);
-
+  const [showAllMonths, setShowAllMonths] = useState(false);
 
   const getMonthName = (month) => {
     const months = [
@@ -29,28 +30,42 @@ export default function Calendar() {
     return months[month];
   };
 
-	useEffect(() => {
-		generateMonthDates();
-	}, [year, month])
+  const generateMonthDates = () => {
+    if (!showAllMonths) {
+      const firstDayOfMonth = new Date(year, month, 1);
+      const startingDay = firstDayOfMonth.getDay();
+      const daysInMonth = new Date(year, month + 1, 0).getDate();
+      const currentMonthDates = [];
+      for (let i = 1; i < startingDay; i++) {
+        currentMonthDates.push("");
+      }
 
-	const generateMonthDates = () => {
-		const firstDayOfMonth = new Date(year,month, 1);
-    const startingDay = firstDayOfMonth.getDay();
-    console.log(startingDay)
-    const daysInMonth = new Date(year, month + 1, 0).getDate();
-		const currentMonthDates = [];
-    for( let i = 1; i < startingDay; i++ ) {
-      currentMonthDates.push('');
+      for (let i = 1; i <= daysInMonth; i++) {
+        currentMonthDates.push(i);
+      }
+      setDates(currentMonthDates);
+    } else {
     }
+  };
+  function generateMonths () {
+      const months= [];
 
-		for(let i = 1; i <= daysInMonth; i++) {
-			currentMonthDates.push(i);
-		}
-		setDates(currentMonthDates);
-	}
+      for (let i = 0; i < 12; i++) {
+        months.push(i);
+      }
+      return months;
+  }
+
+  useEffect(() => {
+    generateMonthDates();
+  }, [year, month]);
+  
+
+
+
 
   const goToNextMonth = () => {
-    if(month === 11) {
+    if (month === 11) {
       setYear(year + 1);
       setMonth(0);
     } else {
@@ -58,22 +73,23 @@ export default function Calendar() {
     }
   };
 
-
-
-
   const getActiveDateClass = (date) => {
     const presentDate = new Date();
     const presentMonth = presentDate.getMonth();
     const presentDateDay = presentDate.getDate();
     const presentYear = presentDate.getFullYear();
-    if(month === presentMonth && presentDateDay === date && year === presentYear) {
-      return 'active';
-    } 
-    return 'inactive';
-  }
+    if (
+      month === presentMonth &&
+      presentDateDay === date &&
+      year === presentYear
+    ) {
+      return "active";
+    }
+    return "inactive";
+  };
 
   const getPrevMonthDates = () => {
-    if(month === 0) {
+    if (month === 0) {
       setYear(year - 1);
       setMonth(11);
     } else {
@@ -81,18 +97,23 @@ export default function Calendar() {
     }
   };
 
-
   const handleDateClick = (date) => {
     setSelectedDate(date);
   };
-
-
+  const toggleShowAllMonths = () => {
+    setShowAllMonths(!showAllMonths);
+  };
+  const handleMonthClick = () => {
+    setMonth[month];
+    setShowAllMonths(!showAllMonths);
+  }
 
   return (
     <div className="calendar-wrapper">
       <header>
         <p className="current-date">{year}</p>
-        <p className="current-date">{getMonthName(month)}</p>
+        <p className="current-month" onClick={toggleShowAllMonths}>{getMonthName(month)}</p>
+
         <div className="icons">
           <span
             onClick={getPrevMonthDates}
@@ -100,33 +121,50 @@ export default function Calendar() {
           >
             chevron_left
           </span>
-          <span
-            onClick={goToNextMonth}
-            className="material-symbols-rounded"
-          >
+          <span onClick={goToNextMonth} className="material-symbols-rounded">
             chevron_right
           </span>
         </div>
       </header>
       <div className="calendar">
-        <ul className="weeks">
-          <li>Mon</li>
-          <li>Tue</li>
-          <li>Wed</li>
-          <li>Thu</li>
-          <li>Fri</li>
-          <li>Sat</li>
-          <li>Sun</li>
-        </ul>
-        <div className="days">
-          <ul>
-						{
-							dates.map((date, index)=> (
-								<li onClick={handleDateClick} className={getActiveDateClass(date) }  key={index}>{date}</li>
-							))
-						}
-						</ul>
-        </div>
+        {showAllMonths ? (
+          <ul className="months">
+                {months.map((month, index) => (
+                  <li
+                    onClick={handleMonthClick}
+                    className='month'
+                    key={index}
+                  >
+                    {getMonthName(month)}
+                  </li>
+                ))}
+          </ul>
+        ) : (
+          <>
+            <ul className="weeks">
+              <li>Mon</li>
+              <li>Tue</li>
+              <li>Wed</li>
+              <li>Thu</li>
+              <li>Fri</li>
+              <li>Sat</li>
+              <li>Sun</li>
+            </ul>
+            <div className="days">
+              <ul>
+                {dates.map((date, index) => (
+                  <li
+                    onClick={handleDateClick}
+                    className={getActiveDateClass(date)}
+                    key={index}
+                  >
+                    {date}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );

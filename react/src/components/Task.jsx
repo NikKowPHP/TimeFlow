9e07e748b274cc543262c65fa299df6a44f843aa
NikKowPHP <nik.kow@outlook.com>
2023-05-29 09:18:25 +0200

    make return data dynamic

--- react/src/components/Task.jsx
+++ react/src/components/Task.jsx
@@ -6,8 +6,28 @@ export default function Task({ onClick, data }) {
   const [showTask, setShowTask] = useState(false);
 
   return (
-    <div className="task-container"  onClick={onClick}>
-      <div className="task-content">{data}</div>
+    <div className="task" onClick={onClick}>
+      <div className="task-header">
+        <div className="task-date">{data.date}</div>
+        <div>
+          <ul>
+            {/* {data.tasks.map((task, index) => (
+              <li key={index}>
+                <span className="task-time">{task.time}</span>
+                <span className="task-title">{task.title}</span>
+              </li>
+            ))} */}
+            <li>
+              <span className="task-time">7-14AM</span>
+              <span className="task-title">Kitchen</span>
+            </li>
+            <li>
+              <span className="task-time">14-15AM</span>
+              <span className="task-title">front</span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }

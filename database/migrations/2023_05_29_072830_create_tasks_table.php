696e5526b9c340c3288992f125740cc5ae471113
NikKowPHP <nik.kow@outlook.com>
2023-05-30 20:48:08 +0200

    define table params and columns of task

--- database/migrations/2023_05_29_072830_create_tasks_table.php
+++ database/migrations/2023_05_29_072830_create_tasks_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->date('date');
+            $table->string('time_start');
+            $table->string('time_end');
+            $table->string('title');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 

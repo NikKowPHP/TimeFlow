a1e25ed3d9b28f667ee1bf6626883a8342794749
NikKowPHP <nik.kow@outlook.com>
2023-05-31 08:53:11 +0200

    make indexByUser method

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Task;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskResource;
 
 class TaskController extends Controller
 {
@@ -12,13 +16,21 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        return TaskResource::collection(
+            Task::query()->orderBy('date', 'desc')
+        );
+    }
+    public function indexByUser($user_id)
+    {
+        return TaskResource::collection(
+            Task::where('user_id', $user_id)->get()
+        );
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreUserRequest $request)
     {
         //
     }
@@ -26,17 +38,17 @@ class TaskController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Task $task)
     {
-        //
+        return new TaskResource($task);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateTaskRequest $request, Task $task)
     {
-        //
+
     }
 
     /**

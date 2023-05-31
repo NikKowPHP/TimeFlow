a6980ca2f6763231ec894db42557a13a278a59c8
NikKowPHP <nik.kow@outlook.com>
2023-05-31 07:47:48 +0200

    make factory and seeder for task

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'user_id' => 1,
+
+            'date' => $this->faker->date(),
+            'time_start' => $this->faker->time(),
+            'time_end' => $this->faker->time(),
+            'title' => $this->faker->title()
+        ];
+    }
+}

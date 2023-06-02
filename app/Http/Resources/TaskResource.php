ce9cc60aa49bdffc45f6c87c13624fea7c1ae52c
NikKowPHP <nik.kow@outlook.com>
2023-06-02 15:17:55 +0200

    format time to appropriate format

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -12,14 +13,18 @@ class TaskResource extends JsonResource
      *
      * @return array<string, mixed>
      */
+
+
     public function toArray(Request $request): array
     {
+        $time_start = Carbon::parse($this->time_start);
+        $time_end = Carbon::parse($this->time_end);
         return [
             'id' => $this->id,
             'user_id' => $this->user_id,
             'date' => $this->date,
-            'time_start' => $this->time_start,
-            'time_end' => $this->time_end,
+            'time_start' => $time_start->format('H:i'),
+            'time_end' => $time_end->format('H:i'),
             'title' => $this->title,
         ];
 

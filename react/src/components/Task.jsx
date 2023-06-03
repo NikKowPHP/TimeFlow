ae43bb3c7146590b00e4400c53ce5a2ff7cda867
NikKowPHP <nik.kow@outlook.com>
2023-06-03 07:31:41 +0200

    make an axiluary component task list

--- react/src/components/Task.jsx
+++ react/src/components/Task.jsx
@@ -1,58 +1,13 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import "../styles/task.css";
 import axiosClient from "../axios-client";
 
-export default function Task({ onClick, data }) {
-  const [showTask, setShowTask] = useState(false);
-  const [tasks, setTasks] = useState([]);
-
-  const getTasks = () => {
-    axiosClient.get('/calendar')
-    .then(({data})=> {
-      setTasks(data.data);
-
-    })
-    .catch(error => {
-      console.error(error);
-      console.log(error);
-    });
-  }
-
-  useEffect(() => {
-    getTasks();
-  }, []);
-
+export default function Task({data}) {
 
   return (
-    <div className="task" onClick={onClick}>
-      <div className="task-header">
-        <div className="task-date">{data.date}</div>
-        <div>
-          <ul>
-            {
-              console.log(tasks)
-            }
-            {tasks.map((task, index) => (
-              <li key={index}>
-                <span className="task-time">{task.time_start} - {task.time_end}</span>
-                <span className="task-title">{task.title}</span>
-              </li>
-            ))}
-
-
-
             <li>
-              <span className="task-time">7-14AM</span>
-              <span className="task-title">Kitchen</span>
+              <span className="task-time">{data.timeStart} - {data.timeEnd}</span>
+              <span className="task-title">{data.title}</span>
             </li>
-            <li>
-              <span className="task-time">14-15AM</span>
-              <span className="task-title">front</span>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
   );
 }
--- react/src/components/TaskList.jsx
+++ react/src/components/TaskList.jsx
@@ -1,27 +1,26 @@
 import React from 'react'
 import axiosClient from '../axios-client'
 import Task from './Task'
+import "../styles/task.css";
 
-export default function TaskList() {
+const TaskList = ({selectedDate, tasksArray = [], user = {}}) => {
 	return (
     <div className="task" >
       <div className="task-header">
         <div className="task-date">{selectedDate}</div>
         <div>
           <ul>
-              <Task />
+						{
+							tasksArray && 
+							tasksArray.map((task, index) => (
+								<Task data={{title: task.title, timeStart: task.time_start, timeEnd: task.time_end}} />
+							))
 
-            <li>
-              <span className="task-time">7-14AM</span>
-              <span className="task-title">Kitchen</span>
-            </li>
-            <li>
-              <span className="task-time">14-15AM</span>
-              <span className="task-title">front</span>
-            </li>
+						}
           </ul>
         </div>
       </div>
     </div>
 	)
 }
+export default TaskList;

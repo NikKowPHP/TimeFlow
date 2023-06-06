6f3ec43afd95c90a62a6930056d5ad406681b501
NikKowPHP <nik.kow@outlook.com>
2023-06-06 11:07:10 +0200

    create isAdmin method in user

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,8 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+    public function isAdmin()
+    {
+        return $this->role->name === 'admin';
+    }
 }

0cf667e8b5585ed13b37ea0743531c38cc0c6467
NikKowPHP <nik.kow@outlook.com>
2023-06-07 18:50:38 +0200

    create a new route to roles

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -34,6 +34,10 @@ const router = createBrowserRouter([
         element: <UserForm key="userUpdate" />,
       },
       {
+        path: "/users/roles",
+        element: <Roles
+      }, 
+      {
         path: "/calendar",
         element: <Calendar />,
         children: [

f70ca55318b81d5d15b6d637c28b51b3491aa518
NikKowPHP <nik.kow@outlook.com>
2023-06-07 18:50:47 +0200

    make component roles

---
+++ react/src/views/Roles.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+export default function Roles() {
+	return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <h1>Users</h1>
+        <Link className="btn-add" to={"/users/new"}>
+          Add new
+        </Link>
+      </div>
+      <div className="card animated fadeInDown">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>User ID</th>
+
+              <th>Username</th>
+              <th>Role</th>
+              <th>Create Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+
+          {loading && (
+            <tbody>
+              <tr>
+                <td colSpan={5} className="text-center">
+                  Loading...
+                </td>
+              </tr>
+            </tbody>
+          )}
+
+          {!loading && (
+            <tbody>
+              {roles.map((u) => (
+                <tr key={u.id}>
+                  <td>{u.id}</td>
+                  <td>{u.name}</td>
+                  <td>{u.email}</td>
+                  <td>{u.created_at}</td>
+                  <td>
+                    <Link
+                      className="btn-edit"
+                      style={{ marginRight: "5px" }}
+                      to={"/users/" + u.id}
+                    >
+                      Edit
+                    </Link>
+                    <button
+                      onClick={(ev) => onDelete(u)}
+                      className="btn-delete"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          )}
+        </table>
+      </div>
+    </div>
+	)
+}

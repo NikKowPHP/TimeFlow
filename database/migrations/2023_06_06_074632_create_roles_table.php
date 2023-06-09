5d8c4a1715eec635e8d279d128bff9d9f7e67fa1
NikKowPHP <nik.kow@outlook.com>
2023-06-09 08:04:45 +0200

    modify table to save only roles names

--- database/migrations/2023_06_06_074632_create_roles_table.php
+++ database/migrations/2023_06_06_074632_create_roles_table.php
@@ -13,10 +13,8 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
             $table->string('role');
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 

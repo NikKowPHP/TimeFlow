b999f42f9f39e1a3e607715d13ca9e450bb3417d
NikKowPHP <nik.kow@outlook.com>
2023-06-10 09:39:35 +0200

    assign default role during signup

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SignupRequest;
 use Illuminate\Support\Facades\Auth;
@@ -12,7 +14,8 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
     //
-    public function signup(SignupRequest $signupRequest) {
+    public function signup(SignupRequest $signupRequest)
+    {
         $data = $signupRequest->validated();
 
         $user = User::create([
@@ -21,25 +24,33 @@ class AuthController extends Controller
             'password' => bcrypt($data['password'])
         ]);
 
+        $default_role = Role::where('role', 'user')->first();
+
+        if ($default_role) {
+            $user->roles()->attach($default_role);
+        }
+
         $token = $user->createToken('main')->plainTextToken;
 
         return response(compact('user', 'token'));
 
     }
-    public function login(LoginRequest $loginRequest) {
-       $credentials = $loginRequest->validated();
-       if(!Auth::attempt($credentials))  {
-        return response([
-            'message' => 'Provided email address or password is incorrect'
-        ]);
-       }
-       $user = Auth::user();
-       $token = $user->createToken('main')->plainTextToken;
-       return response(compact('user', 'token'));
+    public function login(LoginRequest $loginRequest)
+    {
+        $credentials = $loginRequest->validated();
+        if (!Auth::attempt($credentials)) {
+            return response([
+                'message' => 'Provided email address or password is incorrect'
+            ]);
+        }
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        return response(compact('user', 'token'));
     }
-    public function logout(Request $request) {
+    public function logout(Request $request)
+    {
         $user = $request->user();
         $user->currentAccessToken()->delete();
-        return response('', 204); 
+        return response('', 204);
     }
 }

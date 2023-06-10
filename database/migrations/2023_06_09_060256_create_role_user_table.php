e7b13e7303849f4a439ad36f6fce3109fefbf505
NikKowPHP <nik.kow@outlook.com>
2023-06-10 08:03:39 +0200

    update migrations

--- database/migrations/2023_06_09_060256_create_role_user_table.php
+++ database/migrations/2023_06_09_060256_create_role_user_table.php
@@ -11,10 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('role_user', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+
+        if (!Schema::hasTable('role_user')) {
+            Schema::create('role_user', function (Blueprint $table) {
+                $table->unsignedBigInteger('role_id');
+                $table->unsignedBigInteger('user_id');
+                $table->timestamps();
+
+                $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+                $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+                $table->primary(['role_id', 'user_id']);
+            });
+
+        }
     }
 
     /**

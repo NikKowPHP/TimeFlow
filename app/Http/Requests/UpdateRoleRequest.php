45e21175fc550aafa06b2890c0d4d4450ff3c3fa
NikKowPHP <nik.kow@outlook.com>
2023-06-13 15:23:22 +0200

    define rules for updating roles

--- app/Http/Requests/UpdateRoleRequest.php
+++ app/Http/Requests/UpdateRoleRequest.php
@@ -11,7 +11,7 @@ class UpdateRoleRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class UpdateRoleRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'user_id' => 'required',
+            'role_id' => 'required|int|in:1,2,3',
         ];
     }
 }

8dfb0e2bb9036997d04218ba0bcb6f714cce4689
NikKowPHP <nik.kow@outlook.com>
2023-06-21 18:01:48 +0200

    make tooltip component

---
+++ react/src/components/Tooltip.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import "../styles/tooltip.css";
+
+export default function Tooltip({ children}) {
+	const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+
+	const handleOnClick = () => {
+		setIsTooltipVisible(!isTooltipVisible);
+	}
+
+	return (
+		<div className='tooltip-container' onClick={handleOnClick}>
+			{children}
+			{isTooltipVisible && (
+				<div className="tooltip">{content}</div>
+			)}
+		</div>
+	)
+}

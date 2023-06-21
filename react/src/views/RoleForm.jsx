42d93b25e0c757d3c51da8a7ca6fc31500e10a13
NikKowPHP <nik.kow@outlook.com>
2023-06-21 17:47:06 +0200

    handle rendering roles from child

--- react/src/views/RoleForm.jsx
+++ react/src/views/RoleForm.jsx
@@ -1,73 +1,75 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import axiosClient from '../axios-client';
-import CheckboxForm from '../components/CheckboxForm';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import axiosClient from "../axios-client";
+import CheckboxForm from "../components/CheckboxForm";
 
 export default function RoleForm() {
-		const {id} =  useParams();
-		const [user, setUser] = useState({});
-		const [roles, setRoles] = useState([]);
-		const [allRoles, setAllRoles] = useState([]);
-		const [showAllRoleNames, setShowAllRoleNames] = useState(false);
+  const { id } = useParams();
+  const [user, setUser] = useState({});
+  const [roles, setRoles] = useState([]);
+  const [allRoles, setAllRoles] = useState([]);
+  const [showAllRoleNames, setShowAllRoleNames] = useState(false);
 
-		useEffect(() => {
-		if(id) {
-				axiosClient
-				.get(`/roles/${id}`)
-				.then(({data}) => {
-					setUser(data);
-					if(data.roles) {
-						setRoles(data.roles);
-					}
-				})
-		}
-			
-		}, [id])
+  const handleCheckboxFormSubmit = (selectedRoles) => {
+    setRoles(selectedRoles);
+  }
 
-		const getAllRoleNames = () => {
 
-			axiosClient.get('/roles/all')
-			.then(({data}) =>{
-				setAllRoles(data.data);
+  useEffect(() => {
+    if (id) {
+      axiosClient.get(`/roles/${id}`).then(({ data }) => {
+        setUser(data);
+        if (data.roles) {
+          setRoles(data.roles);
+        }
+      });
+    }
+  }, [id]);
 
-			})
-			.catch((error) => {
-				console.error(error);
-			})
+  const getAllRoleNames = () => {
+    axiosClient
+      .get("/roles/all")
+      .then(({ data }) => {
+        setAllRoles(data.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-		}
+  const showRolesToggler = () => {
+    setShowAllRoleNames((prevShowAllRoleNames) => !prevShowAllRoleNames);
+  };
+  useEffect(() => {
+    if (showAllRoleNames) {
+      getAllRoleNames();
+    }
+  }, [showAllRoleNames]);
 
-		const showRolesToggler = () => {
-			setShowAllRoleNames((prevShowAllRoleNames) => !prevShowAllRoleNames);
-		}
-		useEffect(() => {
-			if(showAllRoleNames){
-				getAllRoleNames();
-			}
-		}, [showAllRoleNames]);
+  return (
+    <div className="edit-roles-user-info">
+    
+      <div> userid: {id}</div>
 
-	return (
-		<>
-		<div> userid: {id}</div>
-
-		<div>Username: {user.name}</div>
-
-		{roles && roles.map((role, index) => (
-			<span key={index}>role: {role}</span>
-		))
-		
-		}
-		 {roles && (
-			<button className='btn-add' onClick={showRolesToggler}>Add roles</button>
-    )}
-		{  showAllRoleNames && allRoles && (
-				<CheckboxForm userId={id} takenRoles={roles} checkboxObjectsArray={allRoles}  />
-			// allRoles.map((role, index) => (
-			// 	// <div key={index}>{role.id} {role.role}</div>
-
-			// ))
-		)}
-
-		</>
-	)
+      <div>Username: {user.name}</div>
+      <div>
+        <h3>Actual roles:</h3>
+        {roles && 
+          roles.map((role, index) => <span key={index}>{role} </span>)}
+      </div>
+      {roles && (
+        <button className="btn-add" onClick={showRolesToggler}>
+          Add roles
+        </button>
+      )}
+      {showAllRoleNames && allRoles && (
+        <CheckboxForm
+          userId={id}
+          takenRoles={roles}
+          checkboxObjectsArray={allRoles}
+          onSubmit={handleCheckboxFormSubmit}
+        />
+      )}
+    </div>
+  );
 }

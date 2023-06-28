a07c4052a80ec56399e5669bd336b44b6cdbff27
NikKowPHP <nik.kow@outlook.com>
2023-06-28 07:22:06 +0200

    create interceptors to handle network errors

--- react/src/axios-client.js
+++ react/src/axios-client.js
@@ -1,27 +1,37 @@
-import axios from 'axios';
-const axiosClient =  axios.create({
-	baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
-})
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const axiosClient = axios.create({
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+});
 
 axiosClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("ACCESS_TOKEN");
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
-	const token = localStorage.getItem('ACCESS_TOKEN');
-	config.headers.Authorization = `Bearer ${token}`
-	return config;
-})
-
-axiosClient.interceptors.response.use((response) => {
-return response;
-
-
-}, (error) => {
-	const {response} = error;
-	if(response.status === 401) {
-		localStorage.removeItem('ACCESS_TOKEN');
-	}
-	throw error;
-
-})
-
+axiosClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const { response } = error;
+    if (response) {
+      const { status, data } = response;
+      if (status === 401) {
+        localStorage.removeItem("ACCESS_TOKEN");
+        toast.error("Unathorized request");
+      } else if (status === 422) {
+        toast.error(data.message);
+        console.error("Validation errors:", data.errors);
+      } else {
+        toast.error("An error occurred");
+      }
+    } else {
+			toast.error('Request failed');
+		}
+		throw error;
+  });
 
 export default axiosClient;

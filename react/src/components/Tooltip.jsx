34dc552b48c20efd1442cea032b7d87fec9c9355
NikKowPHP <nik.kow@outlook.com>
2023-07-10 20:41:17 +0200

    calculate center of window size coordinates, apply styles to tooltip to  show depending on position of mouse click

--- react/src/components/Tooltip.jsx
+++ react/src/components/Tooltip.jsx
@@ -3,15 +3,48 @@ import "../styles/tooltip.css";
 
 export default function Tooltip({ children, content, tooltipVisible, onVisibilityChange, classes = '' }) {
 	const [isTooltipVisible, setIsTooltipVisible] = useState(tooltipVisible);
+	const [screenCenter, setScreenCenter] = useState({x: 0, y: 0});
+	const [tooltipPositionClass, setTooltipPositionClass] = useState('tooltip-right') ;
+	// TODO: SHOW TOOLTIP LEFT OR RIGHTb
+
+	useEffect(() => {
+		const calculateScreenCenter = () => {
+			const centerX = window.innerWidth / 2;
+			const centerY = window.innerHeight / 2;
+			setScreenCenter({x: centerX, y:centerY});
+		}
+		calculateScreenCenter();
+
+		const handleResize = () => {
+			calculateScreenCenter();
+		};
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		}
+
+	},[]);
 
 	useEffect(() => {
 		setIsTooltipVisible(tooltipVisible)
 	}, [tooltipVisible]);
 
+	const adjustTooltipPosition = () => {
+		const	mouseCoordinates = getMouseClickCoordinates(event);
+		if(mouseCoordinates.x >  screenCenter.x ) {
+			setTooltipPositionClass('tooltip-left');
+		} else {
+			setTooltipPositionClass('tooltip-right');
+		}
+	}
 
+	const getMouseClickCoordinates = (event) => ({x: event.clientX, y: event.clientY
+	});
 	const handleOnClick = () => {
 		setIsTooltipVisible(!isTooltipVisible);
 		onVisibilityChange(!isTooltipVisible);
+		adjustTooltipPosition();
 	}
 	const handleContentClick = (event) => {
 		event.stopPropagation();
@@ -21,7 +54,7 @@ export default function Tooltip({ children, content, tooltipVisible, onVisibilit
 		<div className='tooltip-container' onClick={handleOnClick}>
 			{children}
 			{isTooltipVisible && (
-				<div className={`tooltip ${classes}`} onClick={handleContentClick}>{content}</div>
+				<div className={`tooltip ${classes} ${tooltipPositionClass}`} onClick={handleContentClick}>{content}</div>
 			)}
 		</div>
 	)

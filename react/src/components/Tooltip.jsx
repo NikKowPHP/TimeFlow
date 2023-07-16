0fc348b4cdf09e657b7790f2e4bce63fd328f17e
NikKowPHP <nik.kow@outlook.com>
2023-07-16 08:40:10 +0200

    make close tooltip when click outside

--- react/src/components/Tooltip.jsx
+++ react/src/components/Tooltip.jsx
@@ -1,61 +1,89 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "../styles/tooltip.css";
+import { useRef } from "react";
 
-export default function Tooltip({ children, content, tooltipVisible, onVisibilityChange, classes = '' }) {
-	const [isTooltipVisible, setIsTooltipVisible] = useState(tooltipVisible);
-	const [screenCenter, setScreenCenter] = useState({x: 0, y: 0});
-	const [tooltipPositionClass, setTooltipPositionClass] = useState('tooltip-right') ;
-	// TODO: SHOW TOOLTIP LEFT OR RIGHTb
+export default function Tooltip({
+  children,
+  content,
+  tooltipVisible,
+  onVisibilityChange,
+  classes = "",
+}) {
+  const [isTooltipVisible, setIsTooltipVisible] = useState(tooltipVisible);
+  const [screenCenter, setScreenCenter] = useState({ x: 0, y: 0 });
+  const [tooltipPositionClass, setTooltipPositionClass] =
+    useState("tooltip-right");
+  const tooltipRef = useRef(null);
 
-	useEffect(() => {
-		const calculateScreenCenter = () => {
-			const centerX = window.innerWidth / 2;
-			const centerY = window.innerHeight / 2;
-			setScreenCenter({x: centerX, y:centerY});
-		}
-		calculateScreenCenter();
+  useEffect(() => {
+    const calculateScreenCenter = () => {
+      const centerX = window.innerWidth / 2;
+      const centerY = window.innerHeight / 2;
+      setScreenCenter({ x: centerX, y: centerY });
+    };
+    calculateScreenCenter();
 
-		const handleResize = () => {
-			calculateScreenCenter();
-		};
-		window.addEventListener('resize', handleResize);
+    const handleResize = () => {
+      calculateScreenCenter();
+    };
+    window.addEventListener("resize", handleResize);
 
-		return () => {
-			window.removeEventListener('resize', handleResize);
-		}
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-	},[]);
+  useEffect(() => {
+    setIsTooltipVisible(tooltipVisible);
+  }, [tooltipVisible]);
 
-	useEffect(() => {
-		setIsTooltipVisible(tooltipVisible)
-	}, [tooltipVisible]);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (tooltipRef.current && !tooltipRef.current.contains(event.target)) {
+        setIsTooltipVisible(false);
+        onVisibilityChange(false);
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.addEventListener("click", handleClickOutside);
+    };
+  }, [onVisibilityChange]);
 
-	const adjustTooltipPosition = () => {
-		const	mouseCoordinates = getMouseClickCoordinates(event);
-		if(mouseCoordinates.x >  screenCenter.x ) {
-			setTooltipPositionClass('tooltip-left');
-		} else {
-			setTooltipPositionClass('tooltip-right');
-		}
-	}
+  const adjustTooltipPosition = () => {
+    const mouseCoordinates = getMouseClickCoordinates(event);
+    if (mouseCoordinates.x > screenCenter.x) {
+      setTooltipPositionClass("tooltip-left");
+    } else {
+      setTooltipPositionClass("tooltip-right");
+    }
+  };
 
-	const getMouseClickCoordinates = (event) => ({x: event.clientX, y: event.clientY
-	});
-	const handleOnClick = () => {
-		setIsTooltipVisible(!isTooltipVisible);
-		onVisibilityChange(!isTooltipVisible);
-		adjustTooltipPosition();
-	}
-	const handleContentClick = (event) => {
-		event.stopPropagation();
-	}
+  const getMouseClickCoordinates = (event) => ({
+    x: event.clientX,
+    y: event.clientY,
+  });
+  const handleOnClick = (event) => {
+    event.stopPropagation();
+    setIsTooltipVisible(!isTooltipVisible);
+    onVisibilityChange(!isTooltipVisible);
+    adjustTooltipPosition();
+  };
+  const handleContentClick = (event) => {
+    event.stopPropagation();
+  };
 
-	return (
-		<div className='tooltip-container' onClick={handleOnClick}>
-			{children}
-			{isTooltipVisible && (
-				<div className={`tooltip ${classes} ${tooltipPositionClass}`} onClick={handleContentClick}>{content}</div>
-			)}
-		</div>
-	)
+  return (
+    <div className="tooltip-container" onClick={handleOnClick} ref={tooltipRef}>
+      {children}
+      {isTooltipVisible && (
+        <div
+          className={`tooltip ${classes} ${tooltipPositionClass}`}
+          onClick={handleContentClick}
+        >
+          {content}
+        </div>
+      )}
+    </div>
+  );
 }

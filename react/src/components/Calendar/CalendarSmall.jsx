6c05a7ec7bc92f8891c0d9276048b370f4464080
NikKowPHP <nik.kow@outlook.com>
2023-07-18 19:36:09 +0200

    adjust small calendar

--- react/src/components/Calendar/CalendarSmall.jsx
+++ react/src/components/Calendar/CalendarSmall.jsx
@@ -1,78 +1,18 @@
-import { useEffect, useState } from "react";
 import "../styles/calendar.css";
-import axiosClient from "../../axios-client";
-import { useLocation, useNavigate } from "react-router-dom";
-import TaskList from "../TaskList";
-import Tooltip from "../tooltips/Tooltip";
-import CalendarMonthly from "./CalendarMonthly";
-import { toast } from "react-toastify";
 
 export default function Calendar() {
-  const currentDate = new Date();
-  const months = generateMonths();
-  const [year, setYear] = useState(currentDate.getFullYear());
-  const [month, setMonth] = useState(currentDate.getMonth());
-  const [dates, setDates] = useState([]);
-
-  const [showMonths, setShowMonths] = useState(false);
-  const [showYears, setShowYears] = useState(false);
-  const [tasks, setTasks] = useState([]);
-  const [allTasks, setAllTasks] = useState([]);
-  const [layout, setLayout] = useState("");
-
-  const [selectedDate, setSelectedDate] = useState(
-    new Date().toLocaleDateString()
-  );
-
-  const navigate = useNavigate();
-  const calendarType = useLocation().pathname;
-
-
-
-
-  const hasTasks = (date) => {
-    const thisDate = modifyDateSql(date);
-    if (allTasks.some((task) => task.date === thisDate)) {
-      return "has-tasks";
-    } else {
-      return "";
-    }
-  };
-
 
   // handle clicks
   const handleDateClick = (date) => {
     const selectedDate = date.toLocaleDateString();
     setSelectedDate(selectedDate);
-    navigate(`/calendar/${convertDateSql(selectedDate)}`);
-  };
-  const handleMonthClick = (selectedMonth) => {
-    setMonth(selectedMonth);
-    setShowMonths(!showMonths);
-  };
-  const handleTaskClick = (taskId, event) => {
-    console.log(taskId);
-    event && event.stopPropagation();
-    setOpenTooltipId(taskId);
   };
 
-  const handleActiveTaskState = (newState) => {
-    setIsTooltipVisible(newState);
-  };
 
-  //toggle views
-  const toggleShowMonths = () => {
-    setShowMonths(!showMonths);
-  };
-  const toggleShowYears = () => {
-    setShowYears(!showYears);
-  };
-
-  // render views
-
+  // render dates of month
   const renderDates = () => {
     return (
-      <>
+      <div className="calendar">
         <ul className={"weeks animated fadeInDown "}>
           <li>Mon</li>
           <li>Tue</li>
@@ -93,24 +33,21 @@ export default function Calendar() {
             </div>
           ))}
         </div>
-        {tasks && <TaskList selectedDate={selectedDate} tasksArray={tasks} />}
-      </>
+      </div>
     );
   };
 
-
+  // render calendar view
   return (
     <div className="calendar-wrapper">
       <header>
         <button
           className="current-date btn-transparent"
-          onClick={toggleShowYears}
         >
           {year}
         </button>
         <button
           className="current-month btn-transparent"
-          onClick={toggleShowMonths}
         >
           {getMonthName(month)}
         </button>
@@ -127,10 +64,7 @@ export default function Calendar() {
           </span>
         </div>
       </header>
-      <div className="calendar">
-        {showMonths && renderMonths()}
-        {!showMonths && renderDates()}
-      </div>
+        {renderDates()}
     </div>
   );
 }

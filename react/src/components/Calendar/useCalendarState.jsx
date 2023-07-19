61701ac6ab0114dd5c8a0d5d408596f1faa814f3
NikKowPHP <nik.kow@outlook.com>
2023-07-19 16:57:31 +0200

    documentate, make functions to fetch tasks

--- react/src/components/Calendar/useCalendarState.jsx
+++ react/src/components/Calendar/useCalendarState.jsx
@@ -1,39 +1,91 @@
 import { useState, useEffect } from "react";
-  const currentDate = new Date();
+import { useLocation } from "react-router-dom";
+import { useCalendarApi } from "./CalendarApiContext";
+import { generateMonthDates } from "./calendarUtils.js";
 
-	// current date
+export function useCalendarState() {
+
+	// State for current date 
   const [year, setYear] = useState(currentDate.getFullYear());
   const [month, setMonth] = useState(currentDate.getMonth());
   const [dates, setDates] = useState([]);
-
-  const months = generateMonths();
-
   const [isTooltipVisible, setIsTooltipVisible] = useState(false);
   const [openTooltipId, setOpenTooltipId] = useState(null);
 
-
-  const [layout, setLayout] = useState("");
-
+  const [layout, setLayout] = useState('');
   const [selectedDate, setSelectedDate] = useState(
     new Date().toLocaleDateString()
   );
 
-  const navigate = useNavigate();
-  const calendarType = useLocation().pathname;
+  // Use the useCalendarApi hook to access the functions
+  const {allTasks, getAllTasks, getTasksOfSelectedDay } = useCalendarApi();
 
+  // Get url pathname
+  const calendarLayout = useLocation().pathname;
 
-	
+  // Set layout depending on the current url 
   useEffect(() => {
-    const modifiedCalendarType = calendarType.replace("/calendar/", "");
-    setLayout(modifiedCalendarType);
-  }, [calendarType]);
+    const calendarType = calendarLayout.replace('/calendar/', '');
+    setLayout(calendarType);
+  }, [calendarLayout])
 
+  // Fetch tasks for the selected date
   useEffect(() => {
-    getTasksOfSelectedDay();
+    fetchTasks(selectedDate);
   }, [selectedDate]);
 
-  // get tasks
+  // Fetch tasks and generate month dates on year and month change
   useEffect(() => {
-    getAllTasks();
-    generateMonthDates();
+    fetchTasksAndGenerateDates();
   }, [year, month]);
+
+  // Function to fetch tasks for the selected date
+  const fetchTasks = (date) => {
+    getTasksOfSelectedDay(date);
+  }
+
+  // Function to fetch tasks and generate month dates
+  const fetchTasksAndGenerateDates = () => {
+    getAllTasks();
+    generateMonthDates(year, month);
+  }
+
+  // Function to calculate the next month
+  const goToNextMonth = () => {
+    if (month === 11) {
+      setYear(year + 1);
+      setMonth(0);
+    } else {
+      setMonth(month + 1);
+    }
+  };
+
+  // Function to calculate the previous month
+  const goToPrevMonth = () => {
+    if (month === 0) {
+      setYear(year - 1);
+      setMonth(11);
+    } else {
+      setMonth(month - 1);
+    }
+  };
+
+	return {
+		year,
+		setYear,
+		month,
+		setMonth,
+		goToNextMonth,
+		goToPrevMonth,
+		dates,
+		setDates,
+		isTooltipVisible,
+		setIsTooltipVisible,
+		openTooltipId,
+		setOpenTooltipId,
+		layout,
+		setLayout,
+		selectedDate,
+		setSelectedDate,
+	};
+}

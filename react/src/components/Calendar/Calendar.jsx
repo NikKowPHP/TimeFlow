9788f4d16f798221529ac2a92bedeb2cb68767da
NikKowPHP <nik.kow@outlook.com>
2023-07-22 06:53:22 +0200

    refactor: import state, move handlers - wrap calendar with apiProvider - handle clicks move to the main component

--- react/src/components/Calendar/Calendar.jsx
+++ react/src/components/Calendar/Calendar.jsx
@@ -1,13 +1,34 @@
+import "../../styles/calendar.css";
 import { useCalendarState } from "./useCalendarState";
 import CalendarMonthly from "./CalendarMonthly";
 import CalendarWeekly from "./CalendarWeekly";
 import CalendarAgenda from "./CalendarAgenda";
+import { CalendarApiProvider } from "./CalendarApiContext";
 
 export function Calendar() {
-  const { layout = "month", dates, openTooltipId, selectedDate} = useCalendarState();
+  const {
+    layout = "month",
+    dates,
+    openTooltipId,
+    setOpenTooltipId,
+    setIsTooltipVisible,
+    selectedDate,
+    getAllTasks,
+    getTasksOfSelectedDay,
+  } = useCalendarState();
+
+  const handleTaskClick = (taskId, event) => {
+    console.log(taskId);
+    event && event.stopPropagation();
+    setOpenTooltipId(taskId);
+  };
+
+  const handleActiveTaskState = (newState) => {
+    setIsTooltipVisible(newState);
+  };
 
   let CalendarLayoutComponent;
-  if(layout === "week") {
+  if (layout === "week") {
     CalendarLayoutComponent = CalendarWeekly;
   } else if (layout === "agenda") {
     CalendarLayoutComponent = CalendarAgenda;
@@ -15,12 +36,16 @@ export function Calendar() {
     CalendarLayoutComponent = CalendarMonthly;
   }
 
-	return (
-    <CalendarLayoutComponent
-      dates={dates}
-      openTooltipId={openTooltipId}
-      selectedDate={selectedDate}
-    />
-  )
-
+  return (
+    <CalendarApiProvider>
+        <CalendarLayoutComponent
+          dates={dates}
+          openTooltipId={openTooltipId}
+          selectedDate={selectedDate}
+          handleTaskClick={handleTaskClick}
+          handleActiveTaskState={handleActiveTaskState}
+          getAllTasks={getAllTasks}
+        />
+    </CalendarApiProvider>
+  );
 }

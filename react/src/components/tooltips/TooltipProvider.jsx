b8556f8c2fcdd1082bce294415b20d4d7160a727
NikKowPHP <nik.kow@outlook.com>
2023-07-28 09:11:32 +0200

    refactor: restructurize TooltipProvider - make open and close functions

--- react/src/components/tooltips/TooltipProvider.jsx
+++ react/src/components/tooltips/TooltipProvider.jsx
@@ -7,17 +7,35 @@ export const useTooltipContext = () => {
 };
 
 export const TooltipProvider = ({ children }) => {
-  const [tooltipVisible, setIsTooltipVisible] = useState(false);
+  const [tooltipVisible, setTooltipVisible] = useState(false);
   const [tooltipContent, setTooltipContent] = useState('');
 
   const [screenCenter, setScreenCenter] = useState({ x: 0, y: 0 });
   const [tooltipPositionClass, setTooltipPositionClass] = useState("tooltip-right");
-  const tooltipRef = useRef(null);
 
   const showTooltip = (content) => {
-    setIsTooltipVisible(true);
+    setTooltipVisible(true);
     setTooltipContent(content);
   };
+
+  const hideTooltip = () => {
+    setTooltipVisible(false);
+    setTooltipContent('');
+  }
+
+  const adjustTooltipPosition = (event) => {
+    const mouseCoordinates = getMouseClickCoordinates(event);
+    if (mouseCoordinates.x > screenCenter.x) {
+      setTooltipPositionClass("tooltip-left");
+    } else {
+      setTooltipPositionClass("tooltip-right");
+    }
+  }
+
+  const getMouseClickCoordinates = (event) => ({
+    x: event.clientX,
+    y: event.clientY,
+  });
 
   useEffect(() => {
     const calculateScreenCenter = () => {
@@ -38,56 +56,32 @@ export const TooltipProvider = ({ children }) => {
     };
   }, []);
 
-  useEffect(() => {
-    setIsTooltipVisible(tooltipVisible);
-  }, [tooltipVisible]);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (tooltipRef.current && !tooltipRef.current.contains(event.target)) {
-        setIsTooltipVisible(false);
-        onVisibilityChange(false);
+        setTooltipVisible(false);
       }
     };
     document.addEventListener("click", handleClickOutside);
     return () => {
       document.addEventListener("click", handleClickOutside);
     };
-  }, [onVisibilityChange]);
+  }, []);
 
-  const adjustTooltipPosition = (event) => {
-    const mouseCoordinates = getMouseClickCoordinates(event);
-    if (mouseCoordinates.x > screenCenter.x) {
-      setTooltipPositionClass("tooltip-left");
-    } else {
-      setTooltipPositionClass("tooltip-right");
-    }
-  }
+  return (
+    <TooltipContext.Provider 
+    value={{
+      tooltipVisible,
+      tooltipContent,
+      showTooltip,
+      hideTooltip,
+    }}
+    >
+    {children}
+    </TooltipContext.Provider>
+  )
 
-  const getMouseClickCoordinates = (event) => ({
-    x: event.clientX,
-    y: event.clientY,
-  });
 
-  const handleTooltipToggle = (event) => {
-    event.stopPropagation();
-    setIsTooltipVisible(!isTooltipVisible);
-    onVisibilityChange(!isTooltipVisible);
-    adjustTooltipPosition(event);
-  };
-  const handleContentClick = (event) => {
-    event.stopPropagation();
-  };
-
-  return {
-    isTooltipVisible,
-    setIsTooltipVisible,
-    tooltipRef,
-    handleTooltipToggle,
-    handleContentClick,
-    tooltipPositionClass,
-
-  }
 
 }
-export default useTooltipState;

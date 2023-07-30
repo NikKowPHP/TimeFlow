c361a0fde024cd9d093de3fe25e9b3841a8172ae
NikKowPHP <nik.kow@outlook.com>
2023-07-30 10:53:39 +0200

    Refactor Tooltip component: - removed default values from children and content, - determined additional states to props - implemented function "handleContentClick" to prevent propagation - defined id to send to useTooltipHook to close tooltip

--- react/src/components/tooltips/Tooltip.jsx
+++ react/src/components/tooltips/Tooltip.jsx
@@ -1,25 +1,45 @@
-import { useEffect, useState, useRef } from "react";
-import PropTypes from "prop-types";
 import "../../styles/tooltip.css";
-import { useRef } from "react";
-import useTooltipState from "./useTooltipState";
 
-export default function Tooltip({children = "",content = "",classes = "",
+/**
+ * Tooltip component is used to display additional information or details when the user interacts with an element.
+ *
+ * @param {Object} props - Component props
+ * @param {React.ReactNode} props.children - The content that will trigger the tooltip on interaction.
+ * @param {React.ReactNode} props.content - The content to be displayed in the tooltip.
+ * @param {string} [props.classes=""] - Additional CSS classes to be applied to the tooltip container.
+ * @param {boolean} props.isTooltipVisible - Determines whether the tooltip is visible or not.
+ * @param {string} props.tooltipPositionClass - The position class for tooltip placement (e.g., "tooltip-right").
+ * @param {string} props.tooltipId - An identifier for the tooltip.
+ *
+ * @returns {JSX.Element} The JSX representation of the Tooltip component.
+ */
+
+export default function Tooltip({
+  children,
+  content,
+  classes = "",
+  isTooltipVisible,
+  tooltipPositionClass,
+  tooltipId,
 }) {
 
-  const {
-    isTooltipVisible,
-    tooltipRef,
-    handleMouseClick,
-    handleTooltipToggle,
-    handleContentClick,
-  } = useTooltipState();
+   /**
+   * Handle click events on the tooltip content to prevent event bubbling.
+   * 
+   * @param {React.MouseEvent} event - The click event object.
+   */
+  
+  const handleContentClick = (event) => {
+    event.stopPropagation();
+  };
 
   return (
-    <div className="tooltip-container" onClick={handleMouseClick} ref={tooltipRef}>
+    <div className="tooltip-container">
       {children}
+
       {isTooltipVisible && (
         <div
+          data-tooltip-id={tooltipId}
           className={`tooltip ${classes} ${tooltipPositionClass}`}
           onClick={handleContentClick}
         >

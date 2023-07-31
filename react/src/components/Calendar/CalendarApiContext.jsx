ffd9462da3bc3bcea58c5907e62f72f58db6015d
NikKowPHP <nik.kow@outlook.com>
2023-07-31 19:56:42 +0200

    refactor: documentate CalendarAPI Context

--- react/src/components/Calendar/CalendarApiContext.jsx
+++ react/src/components/Calendar/CalendarApiContext.jsx
@@ -1,6 +1,20 @@
 import { createContext, useContext, useState } from "react";
 import axiosClient from "../../axios-client";
 
+/**
+ * Calendar API Context
+ * 
+ * This context provides access to the calendar-related data and functions using React's Context API.
+ * 
+ * @typedef {object} calendarApiValue
+ * @property {Array} allTasks - An array containing all tasks.
+ * @property {Array} dayTasks - An array containing all tasks of the selected day.
+ * @property {function} setAllTasks - A function to update the allTasks state.
+ * @property {function} setDayTasks - A function to update the dayTasks state.
+ * @property {function} getAllTasks - A function to fetch all tasks from the API and update the allTasks state.
+ * @property {function} getTasksOfSelectedDay - A function to fetch tasks of the selected date from the API and update the dayTasks state.
+ */
+
 const CalendarApiContext = createContext({
   allTasks: [],
   dayTasks: [],
@@ -10,10 +24,22 @@ const CalendarApiContext = createContext({
   getTasksOfSelectedDay: () => {},
 });
 
+/**
+ * Calendar API Provider Component
+ * 
+ * The CalendarApiProvider is a wrapper component that provides the CalendarApiContext to its child components.
+ * It also contains the state and functions related to calendar data management.
+ * 
+ * @param {object} props - The props for the CalendarApiProvider component.
+ * @param {React.ReactNode} props.children - The child components wrapped by the CalendarApiProvider.
+ * @returns {React.ReactNode} - The JSX element representing the wrapped child components.
+ */
+
 export function CalendarApiProvider({ children }) {
   const [allTasks, setAllTasks] = useState([]);
   const [dayTasks, setDayTasks] = useState([]);
 
+  // Fetch all tasks from the API and update the allTasks state.
   const getAllTasks = () => {
     axiosClient
       .get(`/calendar/calendar`)
@@ -25,6 +51,12 @@ export function CalendarApiProvider({ children }) {
         console.log(error);
       });
   };
+  /**
+   *  Fetch tasks of the selected day from the API and update the dayTasks state.
+   * 
+   * @param {string} selectedDate - The selected date in the format 'YYYY-MM-DD'.
+   * 
+   */
   const getTasksOfSelectedDay = (selectedDate) => {
     axiosClient
       .get(`/calendar/calendar/${selectedDate}`)
@@ -36,7 +68,7 @@ export function CalendarApiProvider({ children }) {
         toast.error("Failed to fetch tasks");
       });
   };
-
+  // The value provided by the context that will be accessible by child components
   const calendarApiValue = {
     allTasks,
     dayTasks,
@@ -53,4 +85,12 @@ export function CalendarApiProvider({ children }) {
   );
 }
 
+/**
+ * Hook to Access Calendar API Context
+ * 
+ * This hook provides easy access to the CalendarApiContext.
+ * 
+ * @returns {CalendarApiValue} - The context value containing calendar-related data and functions.
+ * 
+ */
 export const useCalendarApiContext = () => useContext(CalendarApiContext);

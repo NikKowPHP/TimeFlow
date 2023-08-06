be2d789cf15d27980fde306c8e9da4cad44f0019
NikKowPHP <nik.kow@outlook.com>
2023-08-06 14:09:08 +0200

    feat: create a grid for time selection. - made cells based on currentWeekDates state - styled grid

--- react/src/components/Calendar/CalendarWeekly.jsx
+++ react/src/components/Calendar/CalendarWeekly.jsx
@@ -27,6 +27,36 @@ export default function CalendarWeekly() {
   const handleDateClick = (date) => {
     setSelectedDate(date);
   };
+  const generateHoursOfDay = () => {
+    const hoursOfDay = [];
+    for (let i = 1; i <= 24; i++) {
+      hoursOfDay.push(i);
+    }
+    return hoursOfDay;
+  };
+  const renderTimeGrid = () => {
+    const hoursOfDay = generateHoursOfDay();
+    return (
+      <div className="calendar-weekly__time-list">
+        {hoursOfDay.map((hour) => (
+          <div className="calendar-weekly__time-block" key={hour}>
+            <div className="hour-label">{hour}</div>
+						<div className="time-cells-list">
+							{currentWeekDates && currentWeekDates.map((date, index) => (
+								<div
+									key={index}
+									className={ `calendar-weekly__time-cell ${
+										calendarUtils().getActiveDateClass(date, currentDate, selectedDate)}`}
+										onClick={() => handleDateClick(date)}
+								>
+								</div>
+							))}
+						</div>
+          </div>
+        ))}
+      </div>
+    );
+  };
 
   const renderCurrentWeekDates = () => (
     <div className="calendar-weekly__dates-list">
@@ -50,6 +80,7 @@ export default function CalendarWeekly() {
   return (
     <div className="calendar-weekly__container">
       {currentWeekDates && renderCurrentWeekDates()}
+      {renderTimeGrid()}
     </div>
   );
 }

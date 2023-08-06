c64f02740666e3d13a05a7d148d925f0f6e34f9a
NikKowPHP <nik.kow@outlook.com>
2023-08-06 08:09:39 +0200

    feat: set weekdates state using useEffect and getCurrentWeekDates function from calendarUtils

--- react/src/components/Calendar/CalendarWeekly.jsx
+++ react/src/components/Calendar/CalendarWeekly.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useCalendarState } from './useCalendarState'
+import { calendarUtils } from './calendarUtils';
+
+
 
 export default function CalendarWeekly() {
+	const [currentWeekDates, setCurrentWeekDates] = useState();
+	const {
+		dates,
+		currentDate,
+		allTasks,
+	} = 
+	useCalendarState()
+
+useEffect(() => {
+	if(dates.length != 0) {
+		const currentWeekDates = calendarUtils().getCurrentWeekDates(dates, currentDate);
+		setCurrentWeekDates(currentWeekDates);
+	}
+}, [dates])
+
 	return (
-		<div>CalendarWeekly</div>
+		// <div className='calendar-weekly__container'>
+		// 	{renderCurrentWeek}
+			
+			
+		// </div>
+		<div>
+			nig
+		</div>
 	)
 }

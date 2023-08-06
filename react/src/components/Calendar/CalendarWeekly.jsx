12385d4dd8a50ace7cba11d4107fb1d74ff6c0ba
NikKowPHP <nik.kow@outlook.com>
2023-08-06 11:34:10 +0200

    feat: create calendar header - defined state -fetched week days - handled click to set selectedDate state - rendered a header of a calendar

--- react/src/components/Calendar/CalendarWeekly.jsx
+++ react/src/components/Calendar/CalendarWeekly.jsx
@@ -1,46 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import { useCalendarState } from './useCalendarState'
-import { calendarUtils } from './calendarUtils';
-
-
+import "../../styles/calendar/calendar-weekly.css";
+import React, { useEffect, useState } from "react";
+import { useCalendarState } from "./useCalendarState";
+import { calendarUtils } from "./calendarUtils";
 
 export default function CalendarWeekly() {
-	const [currentWeekDates, setCurrentWeekDates] = useState('');
-	const {
-		dates,
-		currentDate,
-		allTasks,
-	} = 
-	useCalendarState()
+  const [currentWeekDates, setCurrentWeekDates] = useState("");
+  const { dates, currentDate, allTasks, selectedDate, setSelectedDate } =
+    useCalendarState();
 
-useEffect(() => {
-	if(dates.length != 0) {
-		const currentWeekDates = calendarUtils().getCurrentWeekDates(dates, currentDate);
-		setCurrentWeekDates(currentWeekDates);
-	}
-}, [dates])
+  useEffect(() => {
+    if (dates.length != 0) {
+      const currentWeekDates = calendarUtils().getCurrentWeekDates(
+        dates,
+        currentDate
+      );
+      setCurrentWeekDates(currentWeekDates);
+    }
+  }, [dates]);
 
-useEffect(() => {
-	console.log(currentWeekDates)
+  useEffect(() => {
+    console.log(selectedDate);
+  }, [selectedDate]);
 
-}, [currentWeekDates])
+  const weekDays = () => ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
+  const handleDateClick = (date) => {
+    setSelectedDate(date);
+  };
 
-const renderCurrentWeekDates = () => (
-	<div className='calendar-weekly__dates-container'>
-		{
-			currentWeekDates.map((date) => (
-				<div>{date.getDate()}</div>
-			))
-		}
-	</div>
-)
+  const renderCurrentWeekDates = () => (
+    <div className="calendar-weekly__dates-list">
+      {currentWeekDates.map((date, index) => (
+        <div key={index} className="calendar-weekly__date-block">
+          <div className="calendar-weekly__date-block__weekday">
+            {weekDays()[index]}
+          </div>
+          <div
+            className={`calendar-weekly__date-block__date
+      ${calendarUtils().getActiveDateClass(date, currentDate, selectedDate)}`}
+            onClick={() => handleDateClick(date)}
+          >
+            {date.getDate()}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 
-	return (
-		<div className='calendar-weekly__container'>
-			{renderCurrentWeekDates()}
-			
-			
-		</div>
-	)
+  return (
+    <div className="calendar-weekly__container">
+      {currentWeekDates && renderCurrentWeekDates()}
+    </div>
+  );
 }

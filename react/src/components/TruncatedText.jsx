82055a6de54c20102d36741ca5384bec7b180023
NikKowPHP <nik.kow@outlook.com>
2023-08-12 07:24:36 +0200

    feat: create truncatedText component this component serves a purpose of truncating a large text to fit the block element

---
+++ react/src/components/TruncatedText.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TruncatedText({ text, maxCharacters }) {
+	if(text.length > maxCharacters) {
+		const truncatedText = text.substring(0, maxCharacters) + "...";
+		return <span title={text}>{truncatedText}</span>
+	}
+	return <span>{text}</span>
+}
+
+export default TruncatedText;

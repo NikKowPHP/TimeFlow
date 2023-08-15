31016309fcbc3aa852e23f4637aec24cc31db935
NikKowPHP <nik.kow@outlook.com>
2023-08-15 10:24:00 +0200

    feat: create convertTime function that converts hour number to 'HH:MM' format

--- react/src/components/TimeSelection.jsx
+++ react/src/components/TimeSelection.jsx
@@ -1,13 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function TimeSelection({onSelectTime, defaultTime}) {
 
 	const [selectedTime, setSelectedTime] = useState('');
 
 	useEffect(() => {
-		setSelectedTime(defaultTime);
+
+		setSelectedTime(convertTime(defaultTime));
 
 	}, [defaultTime])
+
+	const convertTime = (time) => {
+		const timePad = time.toString().padStart(2, '0');
+		const formattedTime = `${timePad}:00`;
+		return formattedTime;
+	}
 
 	const handleTimeChange = (event) => {
 		const selectedTimeValue = event.target.value;
@@ -18,14 +25,11 @@ export default function TimeSelection({onSelectTime, defaultTime}) {
 
 
 	return (
-		<div className='time-selection-container'>
 			<input 
 			type="time"
 			value={selectedTime}
 			onChange={handleTimeChange}
 			className='time-selection-input'
-			
 			/>
-			</div>
 	)
 }

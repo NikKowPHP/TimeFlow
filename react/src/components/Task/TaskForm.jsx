af3f18f73f615b497fe58642af202370537b68b2
NikKowPHP <nik.kow@outlook.com>
2023-08-27 18:19:49 +0200

    feat: move generic tooltip New Task form functions to TaskForm file: - made accept data from parent; - made send time and date selection data to parent; - sent onTooltip close to parent;

--- react/src/components/Task/TaskForm.jsx
+++ react/src/components/Task/TaskForm.jsx
@@ -1,15 +1,54 @@
 import React from "react";
+import DateSelection from "../DateSelection";
+import TimeSelection from "../TimeSelection";
+import svgPaths from "../svgPaths";
 
-export default function TaskForm() {
-  const handleDateSelection = () => {};
-  const handleTimeSelection = () => {};
+export default function TaskForm({
+  selectedDate,
+  onDateSelection,
+  onTimeSelection,
+  handleTaskCreation,
+  clickedPeriodStart,
+  clickedPeriodEnd,
+  onTooltipClose,
+}) {
 
   /**
-   * Renders the header content for the tooltip.
-   * @returns {JSX.Element} - JSX element containing icons for editing, deleting, and closing the tooltip.
+   * Renders DateSelection component with specified props.
+   * @returns {JSX.Element} JSX element representing the DateSelection component.
    */
-  const tooltipContentHeader = () => (
-    <div className="tooltip-tools">
+  const renderDateSelection = () => (
+    <DateSelection
+      onSelectDate={(newSelectedDate) =>
+        onDateSelection(newSelectedDate)
+      }
+      defaultDate={selectedDate}
+    />
+  );
+
+  /**
+   * Renders TimeSelection component with specified props.
+   * @param {string} time - The time string of the selected cell in format 'HH:MM'.
+   * @param {boolean} isStart - A flag indicating if the time selection is for the start time.
+   * @returns {JSX.Element} JSX element representing the TimeSelection component.
+   */
+  const renderTimeSelection = (defaultTime, isStart) => (
+    <div className="time-selection-block">
+      <TimeSelection
+        onSelectTime={(selectedTime) =>
+          onTimeSelection(selectedTime, isStart)
+        }
+        defaultTime={defaultTime}
+      />
+    </div>
+  );
+
+  /**
+   * Renders the header content for the modal.
+   * @returns {JSX.Element} - JSX element containing icons for editing, deleting, and closing the modal.
+   */
+  const renderModalContentHeader = () => (
+    <div className="modal-tools">
       <svg focusable="false" width="20" height="20" viewBox="0 0 24 24">
         {svgPaths.edit}
       </svg>
@@ -21,7 +60,7 @@ export default function TaskForm() {
       </svg>
       <svg
         style={{ cursor: "pointer" }}
-        onClick={() => hideTooltip()}
+        onClick={() => onTooltipClose()}
         focusable="false"
         width="20"
         height="20"
@@ -33,50 +72,16 @@ export default function TaskForm() {
   );
 
   /**
-   * Renders DateSelection component with specified props.
-   * @param {string} id - The unique id of the cell.
-   * @returns {JSX.Element} JSX element representing the DateSelection component.
+   * Renders the modal content for creating a new task with specified parameters.
+   * @param {JSX.Element} modalContentHeader - The JSX element representing the header of the modal with icons and layout.
+   * @returns {JSX.Element} - The JSX element representing the modal content for creating a new task.
    */
-  const renderDateSelection = (id) => (
-    <DateSelection
-      onSelectDate={(newSelectedDate, id) =>
-        handleDateSelection(newSelectedDate, id)
-      }
-      defaultDate={selectedDate}
-      id={id}
-    />
-  );
-
-  /**
-   * Renders TimeSelection component with specified props.
-   * @param {string} time - The time string of the selected cell in format 'HH:MM'.
-   * @param {boolean} isStart - A flag indicating if the time selection is for the start time.
-   * @returns {JSX.Element} JSX element representing the TimeSelection component.
-   */
-  const renderTimeSelection = (time, isStart) => (
-    <div className="time-selection-block">
-      <TimeSelection
-        onSelectTime={(selectedTime) =>
-          handleTimeSelection(selectedTime, isStart)
-        }
-        defaultTime={time}
-      />
-    </div>
-  );
-
-  /**
-   * Renders the tooltip content for creating a new task with specified parameters.
-   * @param {string} dayName - The name of the day.
-   * @param {string} id - The unique ID of the cell.
-   * @param {JSX.Element} tooltipContentHeader - The JSX element representing the header of the tooltip with icons and layout.
-   * @returns {JSX.Element} - The JSX element representing the tooltip content for creating a new task.
-   */
-  const renderTooltipContentNewTask = (dayName, id, tooltipContentHeader) => (
+  const renderModalNewTaskForm = (modalContentHeader) => (
     <div>
-      {/* render header of tooltip */}
-      {tooltipContentHeader}
+      {/* render header of modal */}
+      {modalContentHeader}
       <form onSubmit={handleTaskCreation}>
-        <div className="tooltip-task-title">
+        <div className="modal-task-title">
           <h2>Create a new event </h2>
           <input
             type="text"
@@ -85,31 +90,30 @@ export default function TaskForm() {
               setTask({ ...task, title: event.target.value })
             }
           />
-          <div className="tooltip-task-time">
-            {renderDateSelection(id)}
+          <div className="modal-task-time">
+            {renderDateSelection()}
 
-            <div className="tooltip-task-time_time-selection-container">
+            <div className="modal-task-time_time-selection-container">
               {renderTimeSelection(clickedPeriodStart, true)}-
               {renderTimeSelection(clickedPeriodEnd, false)}
             </div>
-            <div className="tooltip-task-description-container"></div>
+            <div className="modal-task-description-container"></div>
           </div>
-          <div className="tooltip-task__time-period">
-            <span className="tooltip-task-time__day">{dayName}</span>
+          <div className="modal-task__time-period">
             <span>
               {clickedPeriodStart}-{clickedPeriodEnd}
             </span>
           </div>
         </div>
-        <div className="tooltip-task-additional">
+        <div className="modal-task-additional">
           {/* TODO: create notifications */}
-          <div className="tooltip-task-notification">
+          <div className="modal-task-notification">
             <svg focusable="false" width="20" height="20" viewBox="0 0 24 24">
               {svgPaths.notification}
             </svg>
             <p>in 5 minutes before</p>
           </div>
-          <div className="tooltip-task-owner">
+          <div className="modal-task-owner">
             <i className="fa fa-calendar"></i>
           </div>
           <button className="btn btn-block" type="submit">
@@ -120,32 +124,6 @@ export default function TaskForm() {
     </div>
   );
 
-  /**
-   * Renders a wrapper around the Tooltip component with the provided content.
-   *
-   * @param {string} id- The unique ID of the tooltip.
-   * @param {JSX.Element} tooltipContent - The JSX element representing the content of the tooltip.
-   * @param {JSX.Element} childrenContent - The JSX element representing the content of the cell.
-   * @returns {JSX.Element} JSX element representing the wrapped Tooltip component with content.
-   */
-  const renderTooltipWrapper = (id, tooltipContent, childrenContent) => (
-    <Tooltip
-      isTooltipVisible={openedTooltipId === id}
-      tooltipPositionClass={tooltipPositionClass}
-      classes={`tooltip-task-description ${tooltipPositionClass} `}
-      key={id}
-      content={tooltipContent}
-    >
-      {/* Children */}
-      {childrenContent}
-    </Tooltip>
-  );
 
-  const tooltipContent = renderTooltipContentNewTask(
-    dayName,
-    id,
-    tooltipContentHeader()
-  );
-
-  return renderTooltipWrapper(id, tooltipContent, childrenContent);
+  return renderModalNewTaskForm(renderModalContentHeader());
 }

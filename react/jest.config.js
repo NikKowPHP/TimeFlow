0c8f6797360b1469adddda13bec3c55f7534b4c9
NikKowPHP <nik.kow@outlook.com>
2023-08-30 07:23:38 +0200

    fix: delete jest configuration file to prevent the error, update packages config

--- react/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // Indicates that Jest should use the jsdom environment (browser-like environment) for testing
-  testEnvironment: 'jsdom',
-  // Specifies the file patterns Jest should consider as test files
-  testMatch: ['<rootDir>/**/*.test.js'],
-  // Set up files to run before tests start
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-};

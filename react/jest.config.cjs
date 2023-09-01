3445435f15633925d8fd18b289579615c237aece
NikKowPHP <nik.kow@outlook.com>
2023-09-01 08:56:55 +0200

    feat: make run tests and app - updated babel config file; - updated jest config file with props of testEnviroment, moduleFileExtensions, transform of mjs extensions.

--- react/jest.config.cjs
+++ react/jest.config.cjs
@@ -1,10 +1,15 @@
-
-require = require('esm')(module);
 module.exports = {
   // Use the esm configuration
-  testEnvironment: 'node',
+  // testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+   moduleFileExtensions: ['js', 'jsx', 'json', 'mjs', 'node'],  
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy',
+    '^import\\.meta\\.env$': '<rootDir>/src/mock-import-meta-env.mjs', 
+  },
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.mjs$': 'babel-jest',
   },
   transformIgnorePatterns: [],
 };

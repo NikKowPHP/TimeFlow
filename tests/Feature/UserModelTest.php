98b6f1ff955fa9bac181f1c15a593ac9e924dce7
NikKowPHP <nik.kow@outlook.com>
2023-09-03 09:10:19 +0200

    feat: make tests for User Model

--- tests/Feature/UserModelTest.php
+++ tests/Feature/UserModelTest.php
@@ -3,23 +3,28 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\Models\Role;
 use App\Models\User;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserModelTest extends TestCase
 {
 
-    use RefreshDatabase;
-    public function testUserCanBeCreated(): void
+    public function testCreateUser(): void
     {
-        $user = User::factory()->create([
-            'name' => 'John Doe',
-            'email' => 'john@example.com',
-        ]);
-        $this->assertDatabaseHas('users', [
-            'name' => 'John Doe',
-            'email' => 'john@example.com',
-        ]);
+        $user = User::factory()->create();
+
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertDatabaseHas('users', ['email' => $user->email]);
+    }
+
+    public function testCheckIsUserAdmin()
+    {
+        $user = User::factory()->create();
+        $this->assertFalse($user->isAdmin());
+
+        $admin_user = User::factory()->create();
+        $admin_user->roles()->attach(Role::where('role', 'admin')->first());
+        $this->assertTrue($admin_user->isAdmin());
+
     }
 }

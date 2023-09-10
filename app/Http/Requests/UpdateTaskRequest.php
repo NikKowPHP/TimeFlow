d82c52a688b9a5afe6b4519a01687f3248735593
NikKowPHP <nik.kow@outlook.com>
2023-09-10 08:16:58 +0200

    feat: Define validation rules for task updates
    In this commit, validation rules for task updates have been established to ensure data integrity and prevent erroneous user inputs. This enhancement contributes to a more robust and error-resistant app.

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -11,7 +11,7 @@ class UpdateTaskRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class UpdateTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' =>'required|string',
+            'date' => 'required|string',
+            'time_start' =>'required|string',
+            'time_end' =>'required|string',
         ];
     }
 }

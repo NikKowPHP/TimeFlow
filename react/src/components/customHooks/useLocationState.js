19e31c7c22271dad0826588f4e4065d89745c7eb
NikKowPHP <nik.kow@outlook.com>
2023-09-10 07:47:18 +0200

    refactor: create a custom hook 'useLocationState' that saves previous location. - defined a constant that saves previous location; - defined function 'goBack' that navigates to the previous location.

--- react/src/components/customHooks/useLocationState.js
+++ react/src/components/customHooks/useLocationState.js
@@ -1,22 +1,17 @@
 import { useLocation, useNavigate } from "react-router-dom";
 
+
 export function useLocationState() {
+
   const location = useLocation();
   const navigate = useNavigate();
+  const previousLocation = location.state?.previousLocation || '/calendar';
 
-  function setPreviousRoute() {
-    navigate(location.pathname, {
-      state: { previousRoute: location.pathname },
-    });
+  const goBack = () => {
+    navigate(previousLocation);
   }
 
-  function goBack() {
-    const previousRoute = location.state?.previousRoute || "/calendar";
-    navigate(previousRoute);
-  }
-	function navigateTo(route) {
-		navigate(route);
-	}
+	return { previousLocation: previousLocation, navigate, goBack }
 
-  return { setPreviousRoute, goBack, navigateTo };
+
 }

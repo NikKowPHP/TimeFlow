49a7806e3720e2e4d20a22df56a6bbc895944be7
NikKowPHP <nik.kow@outlook.com>
2023-09-17 08:29:45 +0200

    feat: Add functionality to display existing tasks in a modal
    In this commit, we've implemented a feature that allows users to view existing tasks from the task list in a modal. This enhancement provides users with an easier way to access and review their tasks within the application.

--- react/src/components/Calendar/EllipsisTaskList.jsx
+++ react/src/components/Calendar/EllipsisTaskList.jsx
@@ -1,10 +1,17 @@
 import "../../styles/calendar/ellipsis.css";
 import React from "react";
 import svgPaths from "../svgPaths";
+import ExistingTask from "../Task/ExistingTask";
+import Modal from "../modals/Modal";
 
 export default function ElipsisTaskList({
-	taskList,
+  taskList,
   onModalClose,
+  onTaskDelete,
+  onTaskEdit,
+  openedModalId,
+  modalPositionClass,
+  handleOnTaskClick,
 }) {
   // Render the header of the modal content
   const modalContentHeader = () => (
@@ -21,19 +28,46 @@ export default function ElipsisTaskList({
       </svg>
     </div>
   );
-	const renderTaskList = () => (
-		taskList.map((task) => (
-			<li key={task.id} className="ellipsis-task-list__item">{task.title} ({task.time_start}-{task.time_end})</li>
-		))
-	)
+
+  const modalTaskChildren = (task) => {
+    return (
+      <div
+        onClick={(event) =>
+          handleOnTaskClick({ event: event, modalId: task.id})
+        }
+        key={task.id}
+        className="ellipsis-task-list__item"
+      >
+        {task.title} ({task.time_start}-{task.time_end})
+      </div>
+    );
+  };
+  const renderTaskList = () =>
+    taskList.map((task) => (
+      <Modal
+        classes={`modal-task-description ${modalPositionClass} `}
+        key={task.id}
+        isModalVisible={openedModalId === task.id}
+        modalPositionClass={modalPositionClass}
+        modalId={openedModalId}
+        content={
+          <ExistingTask
+            task={task}
+            onModalClose={onModalClose}
+            onDelete={onTaskDelete}
+            onTaskEdit={onTaskEdit}
+          />
+        }
+      >
+        {modalTaskChildren(task)}
+      </Modal>
+    ));
   const modalContentBody = () => (
     <div>
       {modalContentHeader()}
-			<div className="ellipsis-task-list__body">
-				<ul className="ellipsis-task-list__list">
-					{renderTaskList()}
-				</ul>
-			</div>
+      <div className="ellipsis-task-list__body">
+        <div className="ellipsis-task-list__list">{renderTaskList()}</div>
+      </div>
     </div>
   );
 

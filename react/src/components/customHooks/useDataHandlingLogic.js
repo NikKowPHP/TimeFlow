59f29f5470fc574bbe6a029a00488db07519c564
NikKowPHP <nik.kow@outlook.com>
2023-09-24 07:59:58 +0200

    refactor: Create seperate hook for handling the data from the child component

---
+++ react/src/components/customHooks/useDataHandlingLogic.js
@@ -0,0 +1,20 @@
+import { toast } from 'react-toastify';
+export function useDataHandlingLogic({hideModal, setClickedCellIndex}) {
+	// const {hideModal} = useModalState();
+
+	  /**
+   * Handles data received from a child component (newTaskHandler)
+   * @param {Object} data - Data received from the child component
+   * @returns {any}
+   */
+		function handleDataFromChild(data) {
+			if (data) {
+				hideModal();
+				setClickedCellIndex(null);
+				toast.success(`The task '${data.title}' was successfully created`);
+			}
+		}
+		return {
+			handleDataFromChild,
+		}
+}

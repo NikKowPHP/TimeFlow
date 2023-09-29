52111f8a303048207669e2c1d9e9cb46c31d73ac
NikKowPHP <nik.kow@outlook.com>
2023-09-29 08:17:50 +0200

    feat: Define a command for a laravel tasker to check the due tasks

--- app/Console/Commands/CheckDueTasks.php
+++ app/Console/Commands/CheckDueTasks.php
@@ -3,8 +3,11 @@
 namespace App\Console\Commands;
 
 use App\Models\Task;
+use App\Notifications\DesktopNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
+use App\Http\Resources\TaskResource;
+use Illuminate\Support\Facades\Http;
 
 class CheckDueTasks extends Command
 {
@@ -27,10 +30,32 @@ class CheckDueTasks extends Command
      */
     public function handle()
     {
-        $tasks = Task::where('due_datetime', '>=', now())
-            ->where('due_datetime', '<=', now()->addMinutes(15))
+        $tasks = Task::where('date', '>=', now())
             ->where('notified', 0)
             ->get();
-        Log::debug('Due tasks are comming ', $tasks);
+        foreach ($tasks as $task) {
+            $notificationTime = $this->calculateNotificationTime($task);
+
+            if (now() >= $notificationTime) {
+                $this->sendNotification($task);
+            }
+        }
+    }
+    protected function calculateNotificationTime($task)
+    {
+        if ($task->notification_preference === '1_day_before') {
+            return $task->due_datetime->subDay();
+        }
+        return $task->due_datetime;
+    }
+    protected function sendNotification($task)
+    {
+        $user = $task->user;
+        try {
+
+        } catch (\Exception $e) {
+            $user->notify(new DesktopNotification($task));
+            $task->update(['notified' => 1]);
+        }
     }
 }

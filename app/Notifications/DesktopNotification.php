40ab3ee4fdb16844c5b6c4dc83b2e202845443eb
NikKowPHP <nik.kow@outlook.com>
2023-09-29 08:21:06 +0200

    feat: Implement DesktopNotification Notification model

---
+++ app/Notifications/DesktopNotification.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class DesktopNotification extends Notification
+{
+    use Queueable;
+    protected $task;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($task)
+    {
+        $this->task = $task;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['desktop'];
+    }
+
+    public function toWeb(object $notifiable)
+    {
+        return [
+            'title' => 'Task ' . $this->task->title . ' due soon!',
+            'message' => 'Your task is due ' . $this->task->date . ' and starts ' . $this->task->time_start,
+        ];
+
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    // public function toMail(object $notifiable): MailMessage
+    // {
+    //     return (new MailMessage)
+    //                 ->line('The introduction to the notification.')
+    //                 ->action('Notification Action', url('/'))
+    //                 ->line('Thank you for using our application!');
+    // }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}

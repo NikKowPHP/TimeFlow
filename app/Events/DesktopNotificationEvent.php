c73a76ff4dd6ba8f33e9eb55d6aba6ac336b63af
NikKowPHP <nik.kow@outlook.com>
2023-09-30 08:15:28 +0200

    Make DesktopNotificationEvent to send a notification to the front end

---
+++ app/Events/DesktopNotificationEvent.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class DesktopNotificationEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $title;
+    public $message;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($title, $message)
+    {
+        $this->title = $title;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('notifications'),
+        ];
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'title' => $this->title,
+            'message' => $this->message,
+        ];
+    }
+}

886db2359b1d2bcefe1d769fcc9462ed14d24453
NikKowPHP <nik.kow@outlook.com>
2023-10-02 18:17:51 +0200

    Define predis as a broadcaster

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('BROADCAST_DRIVER', 'null'),
+    'default' => env('BROADCAST_DRIVER', 'redis'),
 
     /*
     |--------------------------------------------------------------------------
@@ -30,23 +30,23 @@ return [
 
     'connections' => [
 
-        'pusher' => [
-            'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
-            'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
-                'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
-            ],
-            'client_options' => [
+        // 'pusher' => [
+        //     'driver' => 'pusher',
+        //     'key' => env('PUSHER_APP_KEY'),
+        //     'secret' => env('PUSHER_APP_SECRET'),
+        //     'app_id' => env('PUSHER_APP_ID'),
+        //     'options' => [
+        //         'cluster' => env('PUSHER_APP_CLUSTER'),
+        //         'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
+        //         'port' => env('PUSHER_PORT', 443),
+        //         'scheme' => env('PUSHER_SCHEME', 'https'),
+        //         'encrypted' => true,
+        //         'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+        //     ],
+            // 'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
-            ],
-        ],
+            // ],
+        // ],
 
         'ably' => [
             'driver' => 'ably',
@@ -57,12 +57,12 @@ return [
             'driver' => 'redis',
             'connection' => 'default',
         ],
-        'socketio' => [
-            'driver' => 'socket.io',
-            'host' => env('SOCKET_IO_HOST', 'localhost'),
-            'port' => env('SOCKET_IO_PORT', 6001),
-            'namespace' => env('SOCKET_IO_NAMESPACE', '/socket.io'),
-        ],
+        // 'socketio' => [
+        //     'driver' => 'socket.io',
+        //     'host' => env('SOCKET_IO_HOST', 'localhost'),
+        //     'port' => env('SOCKET_IO_PORT', 6001),
+        //     'namespace' => env('SOCKET_IO_NAMESPACE', '/socket.io'),
+        // ],
 
         'log' => [
             'driver' => 'log',

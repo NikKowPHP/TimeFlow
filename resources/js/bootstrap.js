8a85654e2debdadf325f1d83903fdf6c29b09c94
NikKowPHP <nik.kow@outlook.com>
2023-10-04 19:34:28 +0200

    Configure pusher in bootstrap.js file

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -16,10 +16,15 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
 import Echo from 'laravel-echo';
-window.io = require('socket.io-client');
+import Pusher from 'pusher-js';
+// window.io = require('socket.io-client');
+window.Pusher = Pusher;
+
 window.Echo = new Echo({
-	broadcaster: 'socket.io',
-	host: window.location.hostname + ':6001',
+	broadcaster: 'pusher',
+	key: import.meta.env.VITE_PUSHER_APP_KEY,
+	cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+	forceTLS: true
 })
 
 // import Pusher from 'pusher-js';

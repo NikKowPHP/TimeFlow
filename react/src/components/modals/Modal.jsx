02bf1068cd4d383eeed948395b873d14f894d1f8
NikKowPHP <nik.kow@outlook.com>
2023-10-09 19:52:09 +0200

    refactor: Move functions of modal dragging to the custom hook

--- react/src/components/modals/Modal.jsx
+++ react/src/components/modals/Modal.jsx
@@ -20,15 +20,11 @@ export default function Modal({
   children,
   content,
   classes = "",
-  style,
   isModalVisible,
   modalId,
   modalRef,
-  position,
+  modalPosition,
 }) {
-  const [dragging, setDragging] = useState(false);
-  const [offset, setOffset] = useState({ x: 0, y: 0 });
-  const [modalPosition, setModalPosition] = useState(position);
 
   /**
    * Handle click events on the modal content to prevent event bubbling.
@@ -40,52 +36,6 @@ export default function Modal({
     event.stopPropagation();
   };
 
-  const handleMouseDown = (event) => {
-    console.log('mouseDown');
-
-    if (!dragging) {
-      const modal = modalRef.current;
-      const modalRect = modal.getBoundingClientRect();
-
-      const offsetX = event.clientX - modalRect.left;
-      const offsetY = event.clientY - modalRect.top;
-      setDragging(true);
-      setOffset({
-        x: offsetX,
-        y: offsetY,
-      });
-    }
-  };
-
-  const handleMouseMove = (event) => {
-    if (dragging) {
-      const left = event.clientX - offset.x;
-      const top = event.clientY - offset.y;
-
-      setModalPosition({ left: `${left}px`, top: `${top}px` });
-    }
-  };
-
-  const handleMouseUp = () => {
-    console.log('mouseUp');
-    if (dragging) {
-      setDragging(false);
-    }
-  };
-
-  useEffect(() => {
-    if (isModalVisible) {
-      modalRef.current.addEventListener("mousedown", handleMouseDown);
-      window.addEventListener("mousemove", handleMouseMove);
-      window.addEventListener("mouseup", handleMouseUp);
-    } else {
-      if (modalRef) {
-        modalRef.current?.removeEventListener("mousedown", handleMouseDown);
-        window.removeEventListener("mousemove", handleMouseMove);
-        window.removeEventListener("mouseup", handleMouseUp);
-      }
-    }
-  }, [isModalVisible]);
 
   return (
     <div className="modal-container">

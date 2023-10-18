df466b3b4d89fb0557ebb1331a92f4497b20c609
NikKowPHP <nik.kow@outlook.com>
2023-10-18 17:19:21 +0200

    feat: Display sorted and grouped tasks by date.
    In this commit we've created functions to sort and then group tasks by their date

--- react/src/components/Calendar/CalendarAgenda.jsx
+++ react/src/components/Calendar/CalendarAgenda.jsx
@@ -1,21 +1,51 @@
-import React from 'react'
-import { useCalendarState } from '../customHooks/useCalendarState'
-import Loading from '../Loading';
+import React, { useEffect, useState } from "react";
+import { useCalendarState } from "../customHooks/useCalendarState";
+import Loading from "../Loading";
 
 export default function CalendarAgenda() {
-	const {loading} = useCalendarState();
+  const { loading, allTasks } = useCalendarState();
+
+  const [groupedTasks, setGroupedTasks] = useState({});
+
+  useEffect(() => {
+		setGroupedTasks(groupTasksByDate());
+  }, [allTasks]);
 
 
+  const sortDaysByOrder = () =>
+    allTasks.sort((a, b) => a.date - b.date).reverse();
 
+  const groupTasksByDate = () =>
+    sortDaysByOrder().reduce((grouped, task) => {
+			const date = task.date;
+			if(!grouped[date]) {
+				grouped[date] = [];
+			}
+			grouped[date].push(task);
+			return grouped;
+		}, {});
 
+  const renderTaskList = () => {
+		if(!groupedTasks) return ;
 
-	const renderMainView = () => loading ? (
-		<Loading />
-	) : (
-		<div className='calendar-agenda-wrapper'>
+		return Object.entries(groupedTasks).map(([date, tasks]) => (
+				<div key={date} className="group-header">{date}
+				{tasks.map(task => (
+					<div key={task.id}>
+						 {task.title}
+					</div>
+				))}
+				</div>
+		));
 
-
-		</div>
-	)
-	return renderMainView();
+	}
+  const renderMainView = () =>
+    loading ? (
+      <Loading />
+    ) : (
+      <div className="calendar-agenda-wrapper">
+        <div>{renderTaskList()}</div>
+      </div>
+    );
+  return renderMainView();
 }

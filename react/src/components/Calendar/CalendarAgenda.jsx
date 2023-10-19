a88b9d601eb211944691148d9f87dce31701f78f
NikKowPHP <nik.kow@outlook.com>
2023-10-19 09:24:29 +0200

    feat: Make basic layout for calendar agenda

--- react/src/components/Calendar/CalendarAgenda.jsx
+++ react/src/components/Calendar/CalendarAgenda.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useCalendarState } from "../customHooks/useCalendarState";
 import Loading from "../Loading";
+import "../../styles/calendar/calendar-agenda.css";
 
 export default function CalendarAgenda() {
   const { loading, allTasks } = useCalendarState();
@@ -8,37 +9,41 @@ export default function CalendarAgenda() {
   const [groupedTasks, setGroupedTasks] = useState({});
 
   useEffect(() => {
-		setGroupedTasks(groupTasksByDate());
+    setGroupedTasks(groupTasksByDate());
   }, [allTasks]);
-
 
   const sortDaysByOrder = () =>
     allTasks.sort((a, b) => a.date - b.date).reverse();
 
   const groupTasksByDate = () =>
     sortDaysByOrder().reduce((grouped, task) => {
-			const date = task.date;
-			if(!grouped[date]) {
-				grouped[date] = [];
-			}
-			grouped[date].push(task);
-			return grouped;
-		}, {});
+      const date = task.date;
+      if (!grouped[date]) {
+        grouped[date] = [];
+      }
+      grouped[date].push(task);
+      return grouped;
+    }, {});
 
   const renderTaskList = () => {
-		if(!groupedTasks) return ;
+    if (!groupedTasks) return;
 
-		return Object.entries(groupedTasks).map(([date, tasks]) => (
-				<div key={date} className="group-header">{date}
-				{tasks.map(task => (
-					<div key={task.id}>
-						 {task.title}
-					</div>
-				))}
-				</div>
-		));
-
-	}
+    return Object.entries(groupedTasks).map(([date, tasks]) => (
+      <div className="calendar-agenda__group-wrapper">
+        <div key={date} className="calendar-agenda__group-date">
+          {date}
+        </div>
+        <div className="calendar-agenda__group-info">
+          {tasks.map((task) => (
+            <div className="calendar-agenda__group-time-title">
+              <div className="calendar-agenda__group-time">{task.time_start}-{task.time_end}</div>
+              <div className="calendar-agenda__group-title"> {task.title}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    ));
+  };
   const renderMainView = () =>
     loading ? (
       <Loading />

bbb19e70b969b495f20d1ec39a3c3b787c46eda8
NikKowPHP <nik.kow@outlook.com>
2023-10-23 18:55:03 +0200

    refactor: Move time and date handlers to newTaskHandler component

--- react/src/components/Calendar/newTaskHandler.js
+++ react/src/components/Calendar/newTaskHandler.js
@@ -2,41 +2,76 @@ import { useEffect, useState } from "react";
 import axiosClient from "../../axios-client";
 import { useStateContext } from "../../contexts/ContextProvider";
 import { useCalendarState } from "../customHooks/useCalendarState";
+import { useModalState } from "../customHooks/useModalState";
 
-export default function newTaskHandler({ onDataReceived }) {
-	const {user} = useStateContext();
-	const {refreshTasks} = useCalendarState();
+export default function newTaskHandler() {
+  const { user } = useStateContext();
+  const { refreshTasks, updateTasks,  setSelectedDate, setClickedCellIndex } = useCalendarState();
 
-	const [task, setTask] = useState({
-		id: null,
-		user_id: user?.id || '',
-		title: '',
-		time_start: '',
-		time_end: '',
-		date: '',
-		notified: false,
-		notification_preference: '',
-		// description: ''
-	})
-	useEffect(() => {
-		setTask({...task, user_id: user?.id});
-	}, [user])
+const [task, setTask] = useState({
+    id: null,
+    user_id: user?.id || "",
+    title: "",
+    time_start: "",
+    time_end: "",
+    date: "",
+    notified: false,
+    notification_preference: "",
+    // description: ''
+  });
+  useEffect(() => {
+    setTask({ ...task, user_id: user?.id });
+  }, [user]);
 
-	const handleTaskCreation = (ev) => {
-		ev.preventDefault();
+	function displaySuccessTaskCreation(data) {
+    if (data) {
+      hideModal();
+      setSelectedDate(null);
+      setClickedCellIndex(null);
+      toast.success(`The task '${data.title}' was successfully created`);
+    }
+  }
 
-		axiosClient.post(`/calendar/calendar`, task)
-		.then(({data}) => {
-			refreshTasks();
-			onDataReceived(data)
+  const handleTaskCreation = (ev) => {
+    ev.preventDefault();
 
-		})
-	}
+    axiosClient.post(`/calendar/calendar`, task).then(({ data }) => {
+      // updateTasks(task);
+      refreshTasks();
+      onDataReceived(data);
+    });
+  };
+  
 
+  /**
+   * Handles time selection and sets state
+   * @param {Date} selectedTime - The selected time
+   * @param {boolean} isStart - Whether is start of the time period or the end
+   * @returns {void}
+   */
+  const handleTimeSelection = (selectedTime, isStart) => {
+    if (isStart) {
+      setClickedPeriodStart(selectedTime);
+      setTask({ ...task, time_start: selectedTime });
+    } else {
+      setClickedPeriodEnd(selectedTime);
+      setTask({ ...task, time_end: selectedTime });
+    }
+  };
 
-	return {
-		task,
-		setTask, 
-		handleTaskCreation
-	}
+  const handleDateSelection = (newSelectedDate) => {
+    const formattedSelectedDate = new Date(newSelectedDate);
+    formattedSelectedDate;
+    setSelectedDate(formattedSelectedDate);
+    setTask({ ...task, date: newSelectedDate });
+  };
+
+  return {
+    task,
+    setTask,
+    handleTaskCreation,
+    handleDateSelection,
+    handleTimeSelection,
+    displaySuccessTaskCreation,
+  };
 }

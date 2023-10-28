b3d29257b2ceebe4083d7e7bbaa24d86642ca508
NikKowPHP <nik.kow@outlook.com>
2023-10-28 07:46:15 +0200

    feat: Make reducers combiner

---
+++ react/src/redux/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import calendarReducer from "./calendarReducer";
+
+const rootReducer = combineReducers({
+  calendar: calendarReducer,
+});
+export default rootReducer;

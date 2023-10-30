727d41acc2ca9c67df6def268b225d45d7d0ed00
NikKowPHP <nik.kow@outlook.com>
2023-10-30 08:42:38 +0100

    feat: Add reset selected date option to reset selected date state and clicked cell index

--- react/src/redux/reducers/calendarReducer.js
+++ react/src/redux/reducers/calendarReducer.js
@@ -9,6 +9,8 @@ const calendarReducer = (state = initialState, action) => {
       return { ...state, selectedDate: action.payload };
     case "CLICK_CELL":
       return { ...state, clickedCellIndex: action.payload };
+    case "RESET_SELECTED_DATE":
+      return { ...state, selectedDate: null, clickedCellIndex: null };
     default:
       return state;
   }

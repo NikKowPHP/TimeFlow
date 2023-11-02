99803f7437c698333bdd9e31809339f592fe00af
NikKowPHP <nik.kow@outlook.com>
2023-11-02 09:28:14 +0100

    feat: Export action type constants, define new actions of setMonth setYear and setMonthDates to generate dates and set the state

--- react/src/redux/actions/calendarActions.js
+++ react/src/redux/actions/calendarActions.js
@@ -1,20 +1,36 @@
-export const SET_ALL_TASKS = "SET_ALL_TASKS";
-export const SELECT_DATE = "SELECT_DATE";
-export const CLICK_CELL = "CLICK_CELL";
-export const RESET_SELECTED_DATE = "RESET_SELECTED_DATE";
-export const SET_LOADING = "SET_LOADING";
+import { calendarUtils } from "../../utils/calendarUtils";
+import {
+  SELECT_DATE,
+  SET_DATES,
+  CLICK_CELL,
+  RESET_SELECTED_DATE,
+  SET_YEAR,
+  SET_MONTH,
+} from "./actionTypes";
 
 export const selectDate = (date) => ({
-  type: "SELECT_DATE",
+  type: SELECT_DATE,
   payload: date,
 });
 export const clickCell = (index) => ({
-  type: "CLICK_CELL",
+  type: CLICK_CELL,
   payload: index,
 });
 export const resetSelectedDate = () => ({
-  type: "RESET_SELECTED_DATE",
+  type: RESET_SELECTED_DATE,
 });
-export const setAllTasks = () => ({
-  type: "SET_ALL_TASKS",
+export const setYear = (year) => ({
+  type: SET_YEAR,
+  payload: year,
 });
+export const setMonth = (month) => ({
+  type: SET_MONTH,
+  payload: month,
+});
+export const setMonthDates = (year, month) => (dispatch) => {
+  const dates = calendarUtils().generateMonthDates(year, month);
+  dispatch({
+    type: SET_DATES,
+    payload: dates,
+  });
+};

6b5e0a180674320e8b253c85a0a6fb6c1152caa3
NikKowPHP <nik.kow@outlook.com>
2023-11-02 09:18:17 +0100

    feat: Define actions types in seperate file for better code organization

---
+++ react/src/redux/actions/actionTypes.js
@@ -0,0 +1,7 @@
+export const SELECT_DATE = "SELECT_DATE";
+export const CLICK_CELL = "CLICK_CELL";
+export const RESET_SELECTED_DATE = "RESET_SELECTED_DATE";
+export const SET_LOADING = "SET_LOADING";
+export const SET_DATES = "SET_DATES";
+export const SET_YEAR = "SET_YEAR";
+export const SET_MONTH = "SET_MONTH";

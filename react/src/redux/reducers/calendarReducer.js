61f4ec08c865d07d322cde343d35cecfb3a99172
NikKowPHP <nik.kow@outlook.com>
2023-11-03 08:54:44 +0100

    feat: Import actionTypes to use constansts instead of strings

--- react/src/redux/reducers/calendarReducer.js
+++ react/src/redux/reducers/calendarReducer.js
@@ -1,25 +1,38 @@
+import {
+  SELECT_DATE,
+  SET_DATES,
+  CLICK_CELL,
+  RESET_SELECTED_DATE,
+  SET_YEAR,
+  SET_MONTH,
+  SET_LAYOUT,
+} from "../actions/actionTypes";
+
 const initialState = {
   selectedDate: null,
   clickedCellIndex: null,
   dates: [],
-  currentDate: new Date(),
+  currentDate: new Date().getTime(),
+  month: new Date().getMonth(),
   year: new Date().getFullYear(),
   layout: "month"
 };
 
 const calendarReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "SELECT_DATE":
+    case SELECT_DATE:
       return { ...state, selectedDate: action.payload };
-    case "CLICK_CELL":
+    case CLICK_CELL:
       return { ...state, clickedCellIndex: action.payload };
-    case "RESET_SELECTED_DATE":
+    case RESET_SELECTED_DATE:
       return { ...state, selectedDate: null, clickedCellIndex: null };
-    case "SET_YEAR":
+    case SET_YEAR:
       return { ...state, year: action.payload };
-    case "SET_DATES":
+    case SET_MONTH:
+      return { ...state, month: action.payload };
+    case SET_DATES:
       return { ...state, dates: action.payload };
-    case "SET_LAYOUT":
+    case SET_LAYOUT:
       return { ...state, layout: action.payload };
     default:
       return state;

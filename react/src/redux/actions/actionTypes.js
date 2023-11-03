74226ca521775a79dfee900f77e82f4f50ba1dfe
NikKowPHP <nik.kow@outlook.com>
2023-11-03 08:57:18 +0100

    feat: Add constant action name of 'set layout'

--- react/src/redux/actions/actionTypes.js
+++ react/src/redux/actions/actionTypes.js
@@ -5,3 +5,4 @@ export const SET_LOADING = "SET_LOADING";
 export const SET_DATES = "SET_DATES";
 export const SET_YEAR = "SET_YEAR";
 export const SET_MONTH = "SET_MONTH";
+export const SET_LAYOUT = "SET_LAYOUT";

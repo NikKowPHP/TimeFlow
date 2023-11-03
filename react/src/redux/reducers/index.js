ee0a47dfe23217a35372a370a316a27ae9136c53
NikKowPHP <nik.kow@outlook.com>
2023-11-03 08:59:06 +0100

    feat: Combine reducers in rootReducer with calendar and tasks

--- react/src/redux/reducers/index.js
+++ react/src/redux/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import calendarReducer from "./calendarReducer";
+import taskReducer from "./taskReducer";
 
 const rootReducer = combineReducers({
   calendar: calendarReducer,
+  tasks: taskReducer
 });
 export default rootReducer;

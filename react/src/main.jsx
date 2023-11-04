ac5fdd9bd434c4d672634df807930d08c8a3a58e
NikKowPHP <nik.kow@outlook.com>
2023-11-04 07:53:59 +0100

    feat: Wrap up main js component with Redux provider

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './styles/index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './router'
-import { ContextProvider } from './contexts/ContextProvider'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import { ContextProvider } from "./contexts/ContextProvider";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ContextProvider>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </ContextProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);

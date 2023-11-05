3bd08f41ab83609d70eeb66c8444ad7305fe50ff
NikKowPHP <nik.kow@outlook.com>
2023-11-05 16:14:07 +0100

    feat: Add action constants to the action types file

--- react/src/redux/actions/actionTypes.js
+++ react/src/redux/actions/actionTypes.js
@@ -6,3 +6,10 @@ export const SET_DATES = "SET_DATES";
 export const SET_YEAR = "SET_YEAR";
 export const SET_MONTH = "SET_MONTH";
 export const SET_LAYOUT = "SET_LAYOUT";
+
+export const FETCH_TASKS_FAILURE = "FETCH_TASKS_FAILURE";
+export const FETCH_TASKS_SUCCESS = "FETCH_TASKS_SUCCESS";
+export const FETCH_TASKS_REQUEST = "FETCH_TASKS_REQUEST";
+
+export const SET_ALL_TASKS = "SET_ALL_TASKS";
+export const UPDATE_TASKS = "UPDATE_TASKS";

95cfb06a81a14f1fbc3a37409b1d103b5822bd92
NikKowPHP <nik.kow@outlook.com>
2023-11-09 06:54:24 +0100

    feat: Handle task deletion from taskUtils file, move task deletion function from calendar monthly component

--- react/src/utils/taskUtils.js
+++ react/src/utils/taskUtils.js
@@ -1,8 +1,9 @@
+import { toast } from "react-toastify";
 import axiosClient from "../axios-client";
 import { dateUtils } from "./dateUtils";
 import { useNavigate } from "react-router-dom";
 
-export function taskUtils({ onStateReceived }) {
+export function taskUtils({ onStateReceived, hideModal, dispatch, deleteTask }) {
   const navigate = useNavigate();
 
   function onTaskEdit(task) {
@@ -10,14 +11,22 @@ export function taskUtils({ onStateReceived }) {
       state: { previousLocation: location.pathname },
     });
   }
+  const handleTaskState = (state) => {
+
+    if (state.status === 204) {
+      hideModal();
+      dispatch(deleteTask(state.task.id));
+      toast.success(`The task '${state.task.title}' was successfully deleted`);
+    }
+
+  }
   // Function to delete the task.
   const onTaskDelete = (task) => {
     console.log(task);
-    axiosClient.delete(`/tasks/${task.id}`).then((data) => {
-      onStateReceived({ ...data, task: task });
+    axiosClient.delete(`/tasks/${task.id}`).then((state) => {
+      handleTaskState({ ...state, task: task });
     });
   };
-
 
   // Function to fetch tasks of a date.
   const getTasksByDate = (date, allTasks) =>
@@ -26,9 +35,15 @@ export function taskUtils({ onStateReceived }) {
         task.date === dateUtils().convertDateSql(date.toLocaleDateString())
     );
 
+  const getNewTaskId = (allTasks) => {
+    const sortedTasks = allTasks.sort((a, b) => a.id - b.id);
+    return sortedTasks[sortedTasks.length - 1].id + 1;
+  };
+
   return {
     onTaskDelete,
     getTasksByDate,
     onTaskEdit,
+    getNewTaskId,
   };
 }

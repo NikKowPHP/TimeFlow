06d8926cd702198a43538d06627cea3cca9eb731
NikKowPHP <nik.kow@outlook.com>
2023-11-10 06:57:55 +0100

    refactor: Prevent from passing not valid date format to the react input element in date selection component

--- react/src/components/DateSelection.jsx
+++ react/src/components/DateSelection.jsx
@@ -1,28 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import "../styles/dateSelection.css"
+import React, { useEffect, useState } from "react";
+import "../styles/dateSelection.css";
 
-export default function DateSelection({onSelectDate, defaultDate}) {
-	const [selectedDate, setSelectedDate] = useState(defaultDate);
-	useEffect(() =>{
-		const formattedDate = defaultDate.toISOString().slice(0,10);
-		setSelectedDate(formattedDate);
+export default function DateSelection({ onSelectDate, defaultDate }) {
+  const [selectedDate, setSelectedDate] = useState("");
+  useEffect(() => {
+    const formattedDate = defaultDate.toISOString().slice(0, 10);
+    setSelectedDate(formattedDate);
+  }, [defaultDate]);
 
-	}, [defaultDate])
-
-	const handleDateChange = (event) => {
-		const  newSelectedDate = event.target.value;
-		setSelectedDate(newSelectedDate);
-		onSelectDate(newSelectedDate);
-	}
-	return (
-
-		<div className="date-selection-container">
+  const handleDateChange = (event) => {
+    const newSelectedDate = event.target.value;
+    setSelectedDate(newSelectedDate);
+    onSelectDate(newSelectedDate);
+  };
+  return (
+    <div className="date-selection-container">
       <input
+        name="date"
         type="date"
         value={selectedDate}
         onChange={handleDateChange}
         className="date-selection-input"
       />
     </div>
-	)
+  );
 }

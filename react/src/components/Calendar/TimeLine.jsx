2348dbb931ef0aa5a9ef4c3a572d407f46c6f7b8
NikKowPHP <nik.kow@outlook.com>
2023-11-13 17:23:48 +0100

    feat: Create timeLine component
    In this commit we have introduced a TimeLine component that renders time line based on the actual time and calculates the line position dynamically.
    
    Steps that have been made:
    - connected to the redux state
    - defined initial state for line position
    - set interval and dispatch the updated time every 5 minutes
    - created updation of linePosition based on received state
    - introduced calculateLinePosition that calculates based on the current time line position accordingly

---
+++ react/src/components/Calendar/TimeLine.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { connect, useDispatch } from "react-redux";
+import { updateCurrentTime } from "../../redux/actions/timeActions";
+
+const TimeLine = ({ currentTime, updateCurrentTime }) => {
+  const [linePositionStyle, setLinePositionStyle] = useState({ top: '1px' });
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+     setInterval(() => {
+      const newTime = new Date();
+      dispatch(updateCurrentTime(newTime));
+    }, 300000);
+  }, [updateCurrentTime]);
+  useEffect(() => {
+    const linePosition = calculateLinePosition();
+		console.log(currentTime)
+    setLinePositionStyle({ top: `${linePosition}px` });
+  }, [currentTime]);
+
+  const calculateLinePosition = () => {
+    const totalHours = 24;
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const percentage = ((hours * 60 + minutes) / (totalHours * 60)) * 45;
+    return percentage;
+  };
+
+  return <div className="current-time-line" style={linePositionStyle}></div>;
+};
+
+const mapStateToProps = (state) => ({
+  currentTime: state.time.currentTime,
+});
+
+const mapDispatchToProps = {
+  updateCurrentTime,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TimeLine);

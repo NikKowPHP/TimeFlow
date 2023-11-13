8480c42c27c4e7d6592f722edf9bef2d0842b5fb
NikKowPHP <nik.kow@outlook.com>
2023-11-13 17:17:43 +0100

    feat: Add timeReducer to combine reducers function

--- react/src/redux/reducers/index.js
+++ react/src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import calendarReducer from "./calendarReducer";
 import taskReducer from "./taskReducer";
+import { timeReducer } from "./timeReducer";
 
 const rootReducer = combineReducers({
   calendar: calendarReducer,
-  tasks: taskReducer
+  tasks: taskReducer,
+  time: timeReducer,
 });
 export default rootReducer;

a34b22127af9960135e9b3286b249871caee70e8
NikKowPHP <nik.kow@outlook.com>
2023-11-17 16:19:45 +0100

    feat: Introduce googleCallback component that handles retrieving token and user info from backend

---
+++ react/src/components/GoogleCallback.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
+import Loading from "./Loading";
+
+function GoogleCallback() {
+  const { setUser, setToken } = useStateContext();
+  const [loading, setLoading] = useState(true);
+  const location = useLocation();
+
+  useEffect(() => {
+    axiosClient
+      .get(`/auth/google/callback${location.search}`, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+      .then((response) => {
+        setLoading(false);
+        const data = response.data;
+        setToken(data.token);
+        setUser(data.user);
+      });
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+}
+
+export default GoogleCallback;

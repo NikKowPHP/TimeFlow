1218c5bfc6cb941d40de63afc5f38b0fe6e9e61e
NikKowPHP <nik.kow@outlook.com>
2023-11-17 16:18:15 +0100

    feat: Introduce 2 functions that handle google auth requests

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SignupRequest;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 
 class AuthController extends Controller
 {
@@ -53,4 +54,44 @@ class AuthController extends Controller
         $user->currentAccessToken()->delete();
         return response('', 204);
     }
+
+    public function redirectToGoogle()
+    {
+        return response()->json([
+            'url' => Socialite::driver('google')->stateless()->redirect()->getTargetUrl(),
+        ]);
+    }
+    public function handleGoogleCallback()
+    {
+        try {
+            $user = Socialite::driver('google')->stateless()->user();
+            
+            $existingUser = User::where('email', $user->email)->first();
+
+            if ($existingUser) {
+                Auth::login($existingUser);
+                $user = Auth::user();
+                $token = $user->createToken('main')->plainTextToken;
+
+                return response(compact('user', 'token'));
+            } else {
+                $newUserData = [
+                    'name' => $user->name,
+                    'email' => $user->email,
+                ];
+                $newUser = User::create($newUserData);
+                $defaultRole = Role::where('role', 'user')->first();
+
+                if ($defaultRole) {
+                    $newUser->roles()->attach($defaultRole);
+                }
+                Auth::login($newUser);
+                $token = $newUser->createToken('main')->plainTextToken;
+                return redirect('/calendar')->with(compact('user', 'token'));
+            }
+        } catch (\Exception $e) {
+            return redirect('/login')->with('error', 'Failed to authenticate with Google');
+        }
+    }
+
 }

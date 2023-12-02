49a479f1d2cf4a6740618035dc7341c0f2ded6ad
NikKowPHP <nik.kow@outlook.com>
2023-12-02 16:53:46 +0100

    feat(TaskItem): Introduce layout differentiation for desktop and mobile
    Implement distinct layout designs for the TaskItem component, catering to both desktop and mobile displays. This addition enhances the component's adaptability, providing optimized layouts for different device screens.

--- react/src/components/Calendar/TaskItem.jsx
+++ react/src/components/Calendar/TaskItem.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import TruncatedText from "../TruncatedText";
 
-export default function TaskItem({task, classes, handleOnTaskClick, styles}) {
-  return (
+export default function TaskItem({task, classes, handleOnTaskClick, styles, isMobileLayout}) {
+  const renderMobileLayout = () => (
+    <div className={classes} onClick={handleOnTaskClick} style={styles}>
+      <span className="calendar-weekly__task-option__title">
+        {<TruncatedText text={task.title} maxCharacters={10} />}
+      </span>
+    </div>
+  )
+
+  const renderDesktopLayout = () => (
+
     <div className={classes} onClick={handleOnTaskClick} style={styles}>
       <span className="calendar-weekly__task-option__title">
         {<TruncatedText text={task.title} maxCharacters={8} />}
@@ -11,5 +20,7 @@ export default function TaskItem({task, classes, handleOnTaskClick, styles}) {
         {task.time_start}-{task.time_end}
       </span>
     </div>
-  );
+
+  )
+  return isMobileLayout ? renderMobileLayout() : renderDesktopLayout();
 }

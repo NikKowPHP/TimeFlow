6747a9e1d57fc9dff74d5c013f6b37b8ebb5e2ce
NikKowPHP <nik.kow@outlook.com>
2023-12-03 07:38:17 +0100

    refactor(useLocationState): Enhance goBack function to return to the previous page
    Update the goBack function within the useLocationState hook to enable navigation back to the previous page. This refactor improves the functionality for seamless navigation within the application.

--- react/src/components/customHooks/useLocationState.js
+++ react/src/components/customHooks/useLocationState.js
@@ -1,18 +1,32 @@
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
-
 export function useLocationState() {
-
   const location = useLocation();
-  const currentLocation = location.pathname;
   const navigate = useNavigate();
-  const previousLocation = location.state?.previousLocation || '/calendar';
+
+  const [previousLocation, setPreviousLocation] = useState(location.pathname);
+  const [currentLocation, setCurrentLocation] = useState(location.pathname);
+
+  useEffect(() => {
+    setCurrentLocation(location.pathname);
+  }, [location]);
+
+  useEffect(() => {
+    if (currentLocation !== location.pathname) {
+      setPreviousLocation(currentLocation);
+      setCurrentLocation(location.pathname);
+    }
+  }, [location.pathname, currentLocation]);
 
   const goBack = () => {
-    navigate(previousLocation);
-  }
+    navigate(-1);
+  };
 
-	return { previousLocation: previousLocation, navigate, goBack, currentLocation }
-
-
+  return {
+    previousLocation: previousLocation,
+    navigate,
+    goBack,
+    currentLocation,
+  };
 }

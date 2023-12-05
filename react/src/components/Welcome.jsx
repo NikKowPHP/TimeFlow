a8a36db65cd88f8f2a85479ba337be87c5a19528
NikKowPHP <nik.kow@outlook.com>
2023-12-05 10:17:40 +0100

    feat(Welcome): Implement main section design for Welcome page
    Introduce design enhancements to the main section of the Welcome page. This addition improves the visual layout and adjusts styles, enhancing the overall presentation and user experience.

--- react/src/components/Welcome.jsx
+++ react/src/components/Welcome.jsx
@@ -3,24 +3,30 @@ import React from "react";
 
 function Welcome() {
   const renderMain = () => (
-    <main class="main">
-      <section class="main__content">
-        <h1 class="main__title">Welcome to TimeFlow</h1>
-        <p class="main__description">
-          Take control of your schedule and productivity.
-        </p>
+    <section class="welcome-main__content">
+      <h1 class="main__title">
+        Take control of your schedule and productivity
+      </h1>
+      <h2 class="main__description">
+        Time planner that can fit your needs
+      </h2>
+      <div className="main__buttons">
+        <button className="btn btn-block btn-main">
+          Try it out
+        </button>
+      </div>
 
-        <section class="features">
-          <h2 class="features__title">Key Features</h2>
-          <ul class="features__list"></ul>
-        </section>
 
-        <section class="coming-soon">
-          <h2 class="coming-soon__title">Coming Soon</h2>
-          <ul class="coming-soon__list"></ul>
-        </section>
+      <section class="features">
+        <h2 class="features__title">Key Features</h2>
+        <ul class="features__list"></ul>
       </section>
-    </main>
+
+      <section class="coming-soon">
+        <h2 class="coming-soon__title">Coming Soon</h2>
+        <ul class="coming-soon__list"></ul>
+      </section>
+    </section>
   );
   return renderMain();
 }

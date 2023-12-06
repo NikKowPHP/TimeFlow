fa274cb9852f80796a89463e1ff2efa160546663
NikKowPHP <nik.kow@outlook.com>
2023-12-06 11:20:47 +0100

    feat(welcome): Implement scroll-triggered visual animation
    Introduce a visual animation triggered by scrolling within the Welcome page. This addition enhances user engagement by implementing an interactive and visually appealing element tied to scroll behavior.

--- react/src/components/Welcome.jsx
+++ react/src/components/Welcome.jsx
@@ -1,7 +1,7 @@
 import "../styles/welcome.css";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import svgPaths from "./svgPaths";
-
+import { Link } from "react-router-dom";
 function Welcome() {
   const featuresList = [
     {
@@ -43,19 +43,49 @@ function Welcome() {
       icon: svgPaths.calendarIntegration,
     },
   ];
-  const renderMain = () => (
-    <section class="welcome-main__content">
-      <h1 class="main__title">
-        Take control of your schedule and productivity
-      </h1>
-      <h2 class="main__description">Time planner that can fit your needs</h2>
-      <div className="main__buttons">
-        <button className="btn btn-block btn-main">Try it out</button>
-      </div>
+  const headerRef = useRef();
 
-      <section class="features">
-        <h2 class="features__title">Key Features</h2>
-        <ul class="features__list">
+  const reveal = () => {
+    const reveals = document.querySelectorAll(".reveal");
+    const windowHeight = window.innerHeight;
+    reveals.forEach((element) => {
+      const elementTop = element.getBoundingClientRect().top;
+      const elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        element.classList.add("section-active");
+      } else {
+        element.classList.remove("section-active");
+      }
+    });
+  };
+  useEffect(() => {
+    if (headerRef.current) {
+      headerRef.current.classList.add("section-active");
+    }
+    window.addEventListener("scroll", reveal);
+    return () => {
+      window.removeEventListener("scroll",reveal);
+    }
+  }, []);
+
+  const renderMain = () => (
+    <section className="welcome-main__content">
+      <div ref={headerRef} className="reveal fade-bottom">
+        <h1 className="main__title">
+          Take control of your schedule and productivity
+        </h1>
+        <h2 className="main__description">
+          Time planner that can fit your needs
+        </h2>
+        <div className="main__buttons">
+          <Link to="/login" className="btn btn-block btn-main">
+            Try it out
+          </Link>
+        </div>
+      </div>
+      <section className="features reveal fade-right">
+        <h2 className="features__title">Key Features</h2>
+        <ul className="features__list">
           {featuresList.map((feature, index) => (
             <li key={index} className="features__item">
               <span className="feature__icon">
@@ -70,16 +100,17 @@ function Welcome() {
         </ul>
       </section>
 
-      <section class="coming-soon">
+      <section className="coming-soon reveal fade-left">
         <h2 className="coming-soon__title">Coming soon features</h2>
         <ul className="coming-soon__list">
           {comingSoonList.map((item, index) => (
             <li className="coming-soon__item" key={index}>
               <span className="coming-soon__icon">
-                <span className="coming-soon__icon--container">{item.icon}</span>
+                <span className="coming-soon__icon--container">
+                  {item.icon}
+                </span>
               </span>
               <h3 className="coming-soon__item__title">{item.title}</h3>
-              
             </li>
           ))}
         </ul>
